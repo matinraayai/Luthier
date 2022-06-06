1269f6dc68990f9b138c773b6e8a74a29d6f4b81
Yuhui Bao <julie.bao32@gmail.com>
2022-06-06 14:33:17 -0400

    modify sibir.py

--- sibir.py
+++ sibir.py
@@ -4,26 +4,29 @@ import os
 import argparse
 import subprocess
 
-parser = argparse.ArgumentParser(description=
-    '''Sibir is a binary instrumentation tool for AMD GPUs.''')
-parser.add_argument('commands', metavar='commands', nargs='+',
-    help='Commands to execute')
+parser = argparse.ArgumentParser(
+    description="""Sibir is a binary instrumentation tool for AMD GPUs."""
+)
+parser.add_argument(
+    "commands", metavar="commands", nargs="+", help="Commands to execute"
+)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     args = parser.parse_args()
-    command = ' '.join(args.commands)
+    command = " ".join(args.commands)
 
     path = os.path.dirname(os.path.realpath(__file__))
     current_wd = os.getcwd()
 
     env = os.environ.copy()
-    if 'LD_LIBRARY_PATH' not in env:
-        env['LD_LIBRARY_PATH'] = ''
-    env['LD_LIBRARY_PATH'] = path + '/lib' + ':' + env['LD_LIBRARY_PATH']
-    env['LD_PRELOAD'] = path + '/lib/libsibir.so'
+    if "LD_LIBRARY_PATH" not in env:
+        env["LD_LIBRARY_PATH"] = ""
+    env["LD_LIBRARY_PATH"] = path + "/build/lib" + ":" + env["LD_LIBRARY_PATH"]
+    env["LD_PRELOAD"] = path + "/build/lib/libsibir.so"
 
-    proc = subprocess.Popen(command, 
-        shell=True, 
+    proc = subprocess.Popen(
+        command,
+        shell=True,
         cwd=current_wd,
         env=env,
     )
