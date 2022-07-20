5f5ddc09456d93cb2943f0472cc71385f26ccd7f
Yuhui Bao <julie.bao32@gmail.com>
2022-07-20 18:21:31 -0400

    InstType in decodeTable is value now

--- disassembler/decodetable.cpp
+++ disassembler/decodetable.cpp
@@ -2,5 +2,5 @@
 #include <memory>
 void Disassembler::initializeDecodeTable()
 {
-	addInstType(new InstType("s_add_u32", 0, FormatTable[SOP2], 0, ExeUnitScalar, 32, 32, 32, 0, 0));
+	addInstType(InstType("s_add_u32", 0, FormatTable[SOP2], 0, ExeUnitScalar, 32, 32, 32, 0, 0));
 }
