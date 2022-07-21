32b3081f09d3a28623676164c2d98373ce93f9c2
Yuhui Bao <julie.bao32@gmail.com>
2022-07-21 18:15:41 -0400

    open a elf file in main

--- disassembler/main.cpp
+++ disassembler/main.cpp
@@ -1,11 +1,34 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 #include "initialize.h"
 #include "operand.h"
-int main()
+int main(int argc, char *argv[])
 {
+	if (argc != 2)
+	{
+		printf("a input elf file is required\n");
+		return 1;
+	}
+	std::string filename = argv[1];
+	std::streampos size;
+	char *blob;
+	std::ifstream file(filename, std::ios::in | std::ios::binary | std::ios::ate);
+	if (file.is_open())
+	{
+		size = file.tellg();
+		blob = new char[size];
+		file.seekg(0, std::ios::beg);
+		file.read(blob, size);
+		file.close();
+	}
+	else
+	{
+		printf("unable to open file\n");
+		return 1;
+	}
 	initRegs();
+	initFormatTable();
 
-	std::cout << newVRegOperand(0, 0, 1).reg.name << "\n";
-	// std::cout << VReg(0)->name << "\n";
 	return 0;
 }
