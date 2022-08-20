c87a6d9b83fd309c939a93a076ebd111f6e79973
Yuhui Bao <julie.bao32@gmail.com>
2022-08-20 18:10:39 -0400

    add instrumentation/instCount.cpp kernel and processBuddle function

--- src/internal.cc
+++ src/internal.cc
@@ -20,30 +20,31 @@ char *getNoteSection2(elfio::File *elf);
 void editNoteSectionData(elfio::Note &note);
 void editTextSectionData(elfio::File *elf);
 
-uint64_t getHeaderSize(__ClangOffloadBundleHeader *header) {
-  char *blob = reinterpret_cast<char *>(header);
-  auto offset = blob + sizeof(CLANG_OFFLOAD_BUNDLER_MAGIC) - 1 + 8;
-  std::cout << "num of bundles: " << header->numBundles << "\n";
-  const __ClangOffloadBundleDesc *desc =
-      reinterpret_cast<__ClangOffloadBundleDesc *>(offset);
-  uint64_t totalCoSize = 0;
-  for (int i = 0; i < header->numBundles; i++, desc = desc->next()) {
-    totalCoSize += desc->size;
-    uint64_t trippleSize = desc->tripleSize;
-    offset += 8 + 8 + 8 + trippleSize;
-    std::string triple{desc->triple, desc->tripleSize};
-    std::cout << "triple name is " << triple << "\n";
-    std::cout << "desc size is " << desc->size << "\n";
-    printf("desc struct is stored from address%p\n", (void *)desc);
-    printf("bundle triple name is stored from address %p\n", desc->triple);
-    printf("bundle %d offset is %p\n", i, (void *)desc->offset);
-    printf("address after %d th desc is %p\n", i, (void *)offset);
-    // desc = reinterpret_cast<__ClangOffloadBundleDesc *>(offset);
-  }
-  return 0;
-}
+// uint64_t getHeaderSize(__ClangOffloadBundleHeader *header) {
+//   char *blob = reinterpret_cast<char *>(header);
+//   auto offset = blob + sizeof(CLANG_OFFLOAD_BUNDLER_MAGIC) - 1 + 8;
+//   std::cout << "num of bundles: " << header->numBundles << "\n";
+//   const __ClangOffloadBundleDesc *desc =
+//       reinterpret_cast<__ClangOffloadBundleDesc *>(offset);
+//   uint64_t totalCoSize = 0;
+//   for (int i = 0; i < header->numBundles; i++, desc = desc->next()) {
+//     totalCoSize += desc->size;
+//     uint64_t trippleSize = desc->tripleSize;
+//     offset += 8 + 8 + 8 + trippleSize;
+//     std::string triple{desc->triple, desc->tripleSize};
+//     std::cout << "triple name is " << triple << "\n";
+//     std::cout << "desc size is " << desc->size << "\n";
+//     printf("desc struct is stored from address%p\n", (void *)desc);
+//     printf("bundle triple name is stored from address %p\n", desc->triple);
+//     printf("bundle %d offset is %p\n", i, (void *)desc->offset);
+//     printf("address after %d th desc is %p\n", i, (void *)offset);
+//     // desc = reinterpret_cast<__ClangOffloadBundleDesc *>(offset);
+//   }
+//   return 0;
+// }
 
-extern "C" std::vector<hipModule_t> *__hipRegisterFatBinary(char *data) {
+extern "C" std::vector<hipModule_t> *__hipRegisterFatBinary(char *data)
+{
   printf("Here in %s\n", __FUNCTION__);
 
   char *data_copy = new char[sizeof(__CudaFatBinaryWrapper)];
@@ -65,7 +66,8 @@ extern "C" std::vector<hipModule_t> *__hipRegisterFatBinary(char *data) {
       reinterpret_cast<__ClangOffloadBundleDesc *>(offset);
   elfio::File elfFile;
   uint64_t endOfHeader;
-  for (int i = 0; i < header->numBundles; i++, desc = desc->next()) {
+  for (int i = 0; i < header->numBundles; i++, desc = desc->next())
+  {
     printf("desc struct is stored from address%p\n", (void *)desc);
     uint64_t trippleSize = desc->tripleSize;
     offset += 8 + 8 + 8 + trippleSize;
@@ -79,7 +81,8 @@ extern "C" std::vector<hipModule_t> *__hipRegisterFatBinary(char *data) {
     char *codeobj = reinterpret_cast<char *>(
         reinterpret_cast<uintptr_t>(header) + desc->offset);
 
-    if (i == header->numBundles - 1) {
+    if (i == header->numBundles - 1)
+    {
       endOfHeader = (uint64_t)codeobj + coSize;
       printf("address at the end of the last codeobject is %p\n",
              (void *)endOfHeader);
@@ -89,10 +92,12 @@ extern "C" std::vector<hipModule_t> *__hipRegisterFatBinary(char *data) {
   std::memcpy(header_copy, header, endOfHeader - (uint64_t)header);
   offset = (uint64_t)header_copy + sizeof(CLANG_OFFLOAD_BUNDLER_MAGIC) - 1 + 8;
   desc = reinterpret_cast<__ClangOffloadBundleDesc *>(offset);
-  for (int i = 0; i < header->numBundles; i++, desc = desc->next()) {
+  for (int i = 0; i < header->numBundles; i++, desc = desc->next())
+  {
     uint64_t trippleSize = desc->tripleSize;
     std::string triple{desc->triple, desc->tripleSize};
-    if (triple.compare(curr_target)) {
+    if (triple.compare(curr_target))
+    {
       continue;
     }
     std::cout << "matching triple name is " << triple << "\n";
@@ -102,9 +107,9 @@ extern "C" std::vector<hipModule_t> *__hipRegisterFatBinary(char *data) {
     elfFile = elfFile.FromMem(codeobj); // load elf file from code object
   }
 
-  elfio::Note noteSec = getNoteSection(&elfFile);
+  // elfio::Note noteSec = getNoteSection(&elfFile);
 
-  editNoteSectionData(noteSec);
+  // editNoteSectionData(noteSec);
   // editTextSectionData(&elfFile);
   reinterpret_cast<__CudaFatBinaryWrapper *>(data_copy)->binary =
       reinterpret_cast<__ClangOffloadBundleHeader *>(header_copy);
@@ -131,7 +136,8 @@ extern "C" std::vector<hipModule_t> *__hipRegisterFatBinary(char *data) {
 }
 
 std::vector<hipModule_t> *
-call_original_hip_register_fat_binary(const void *data) {
+call_original_hip_register_fat_binary(const void *data)
+{
   std::vector<hipModule_t> *(*func)(const void *);
   func = (decltype(func))dlsym(RTLD_NEXT, "__hipRegisterFatBinary");
 
@@ -144,29 +150,35 @@ call_original_hip_register_fat_binary(const void *data) {
 // Uses the same algorithm that getKernelArgumentMetaData in Rhipo uses.
 // By passing elfio::Note.desc into nlohmann::json::from_msgpack(), you can
 // get the note section as a JSON file. elfio::Note.desc is just a big string.
-elfio::Note getNoteSection(elfio::File *elf) {
+elfio::Note getNoteSection(elfio::File *elf)
+{
   printf("Here in %s\n", __FUNCTION__);
 
   auto note_section = elf->GetSectionByType("SHT_NOTE");
-  if (!note_section) {
+  if (!note_section)
+  {
     panic("note section is not found");
   }
 
   char *blog = note_section->Blob();
   int offset = 0;
-  while (offset < note_section->size) {
+  while (offset < note_section->size)
+  {
     auto note = std::make_unique<elfio::Note>(elf, blog + offset);
     offset += note->TotalSize();
-    if (note->name.rfind("AMDGPU") == 0) {
+    if (note->name.rfind("AMDGPU") == 0)
+    {
       printf("Offset %d\n", offset);
       printf("Total Size %d\n", note->TotalSize());
       return elfio::Note(elf, note->Blob());
     }
   }
 }
-void editTextSectionData(elfio::File *elf) {
+void editTextSectionData(elfio::File *elf)
+{
   auto text_section = elf->GetSectionByName(".text");
-  if (!text_section) {
+  if (!text_section)
+  {
     panic("text section is not found");
   }
   std::cout << "text section size is " << text_section->size << "\n";
@@ -189,7 +201,8 @@ void editTextSectionData(elfio::File *elf) {
 // obj and passes the desc param it into a nlohmann::json obj. Then this edits
 // the desc param, and passes that back into the elfio::Note obj, which is why
 // we pass the note obj by reference.
-void editNoteSectionData(elfio::Note &note) {
+void editNoteSectionData(elfio::Note &note)
+{
   printf("Here in %s\n", __FUNCTION__);
   auto json = nlohmann::json::from_msgpack(note.desc);
   std::cout << std::setw(4) << json << '\n';
