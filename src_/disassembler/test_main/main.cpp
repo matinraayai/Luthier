0f66ff1fb4b847c3af084982ff16b8f503282543
Yuhui Bao <julie.bao32@gmail.com>
2022-09-06 15:27:56 -0400

    amend the mistakenly deleted line

--- src_/disassembler/test_main/main.cpp
+++ src_/disassembler/test_main/main.cpp
@@ -1,59 +1,50 @@
-#include <iostream>
+#include "disassembler.h"
+#include "elf.hpp"
 #include <fstream>
+#include <iostream>
 #include <string>
 #include <vector>
-#include "elf.hpp"
-#include "disassembler.h"
 
-int main(int argc, char *argv[])
-{
-	if (argc != 2)
-	{
-		printf("a input elf file is required\n");
-		return 1;
-	}
-	std::string filename = argv[1];
-	std::string filename_elf;
+int main(int argc, char *argv[]) {
+  if (argc != 2) {
+    printf("a input elf file is required\n");
+    return 1;
+  }
+  std::string filename = argv[1];
+  std::string filename_elf;
 
-	initFormatTable();
+  initRegs();
+  initFormatTable();
 
-	if (filename.find("csv") != std::string::npos)
-	{
-		filename_elf = filename.substr(0, filename.length() - 6);
-	}
-	else
-	{
-		filename_elf = filename;
-	}
-	std::streampos size;
-	char *blob;
-	std::ifstream file(filename_elf, std::ios::in | std::ios::binary | std::ios::ate);
-	if (file.is_open())
-	{
-		size = file.tellg();
-		blob = new char[size];
-		file.seekg(0, std::ios::beg);
-		file.read(blob, size);
-		file.close();
-	}
-	else
-	{
-		printf("unable to open file\n");
-		return 1;
-	}
-	elfio::File elfFile;
-	elfFile = elfFile.FromMem(blob);
-	elfFile.PrintSymbolsForSection(".text");
+  if (filename.find("csv") != std::string::npos) {
+    filename_elf = filename.substr(0, filename.length() - 6);
+  } else {
+    filename_elf = filename;
+  }
+  std::streampos size;
+  char *blob;
+  std::ifstream file(filename_elf,
+                     std::ios::in | std::ios::binary | std::ios::ate);
+  if (file.is_open()) {
+    size = file.tellg();
+    blob = new char[size];
+    file.seekg(0, std::ios::beg);
+    file.read(blob, size);
+    file.close();
+  } else {
+    printf("unable to open input file\n");
+    return 1;
+  }
+  elfio::File elfFile;
+  elfFile = elfFile.FromMem(blob);
+  elfFile.PrintSymbolsForSection(".text");
 
-	Disassembler d(&elfFile);
-	if (filename.find("csv") != std::string::npos)
-	{
-		d.Disassemble(&elfFile, filename);
-	}
-	else
-	{
-		d.Disassemble(&elfFile, filename, std::cout);
-	}
+  Disassembler d(&elfFile);
+  if (filename.find("csv") != std::string::npos) {
+    d.Disassemble(&elfFile, filename);
+  } else {
+    d.Disassemble(&elfFile, filename, std::cout);
+  }
 
-	return 0;
+  return 0;
 }
