8e6088c6ff6f515223f3e13e75f28a3d0feda765
matinraayai <30674652+matinraayai@users.noreply.github.com>
2023-07-07 15:03:22 -0400

    Added caching of GPU hsa agent isas in disassembler.cpp.

--- src/sibir/disassembler.cpp
+++ src/sibir/disassembler.cpp
@@ -1,54 +1,7 @@
 #include "disassembler.h"
 #include "hsa_intercept.h"
 
-//
-//const int ExpectedUserData;
-//
-//void checkUserData(void *UserData) {
-//    if (UserData != (void *)&ExpectedUserData) {
-//        fail("user_data changed");
-//    }
-//}
-//
-//const char *skipspace(const char *S) {
-//    while (isspace(*S)) {
-//        ++S;
-//    }
-//    return S;
-//}
-//
-//size_t strlenWithoutTrailingWhitespace(const char *S) {
-//    size_t I = strlen(S);
-//    while (I && isspace(S[--I])) {
-//        ;
-//    }
-//    return I + 1;
-//}
-//
-//const char Program[] = {
-//    '\x02', '\x00', '\x06', '\xC0', '\x00', '\x00', '\x00', '\x00', '\x7f',
-//    '\xC0', '\x8c', '\xbf', '\x00', '\x80', '\x12', '\xbf', '\x05', '\x00',
-//    '\x85', '\xbf', '\x00', '\x02', '\x00', '\x7e', '\xc0', '\x02', '\x04',
-//    '\x7e', '\x01', '\x02', '\x02', '\x7e', '\x00', '\x80', '\x70', '\xdc',
-//    '\x00', '\x02', '\x7f', '\x00', '\x00', '\x00', '\x81', '\xbf',
-//};
-//
-//const char *Instructions[] = {
-//    "s_load_dwordx2 s[0:1], s[4:5], 0x0",
-//    "s_waitcnt lgkmcnt(0)",
-//    "s_cmp_eq_u64 s[0:1], 0",
-//    "s_cbranch_scc1 5",
-//    "v_mov_b32_e32 v0, s0",
-//    "v_mov_b32_e32 v2, 64",
-//    "v_mov_b32_e32 v1, s1",
-//    "global_store_dword v[0:1], v2, off",
-//    "s_endpgm",
-//};
-//const size_t InstructionsLen = sizeof(Instructions) / sizeof(*Instructions);
-//size_t InstructionsIdx = 0;
-//const size_t BrInstructionIdx = 3;
-//const size_t BrInstructionAddr = 40;
-//
+
 uint64_t readMemoryCallback(sibir_address_t from, char *to, size_t size,
                             void *userData) {
     bool isEndOfProgram = reinterpret_cast<std::pair<std::string, bool>*>(userData)->second;
@@ -82,21 +35,66 @@ void printAddressCallback(uint64_t Address, void *UserData) {
 //    }
 }
 
+hsa_status_t Disassembler::populateAgentInfo(hsa_agent_t agent, Disassembler::hsa_agent_entry_t& entry) {
+    const auto& coreApi = SibirHsaInterceptor::Instance().getSavedHsaTables().core;
+    hsa_status_t status;
 
-hsa_status_t Disassembler::initGpuAgents() {
+    // Get the name (architecture) of the agent
+    std::string agentName;
+    agentName.resize(64);
+
+    coreApi.hsa_agent_get_info_fn(agent, HSA_AGENT_INFO_ISA, agentName.data());
+
+    // Get the Isa name of the agent
+    std::vector<std::string> supportedAgentIsaNames;
+
+    auto getIsaNameCallback = [](hsa_isa_t isa, void* data){
+        auto out = reinterpret_cast<std::vector<std::string>*>(data);
+        auto coreApi = SibirHsaInterceptor::Instance().getSavedHsaTables().core;
+        hsa_status_t status = HSA_STATUS_ERROR;
+        uint32_t isaNameSize;
+        status = SIBIR_HSA_CHECK(coreApi.hsa_isa_get_info_alt_fn(isa, HSA_ISA_INFO_NAME_LENGTH, &isaNameSize));
+        if (status != HSA_STATUS_SUCCESS)
+            return status;
+        std::string isaName;
+        isaName.resize(isaNameSize);
+        status = SIBIR_HSA_CHECK(coreApi.hsa_isa_get_info_alt_fn(isa, HSA_ISA_INFO_NAME, isaName.data()));
+        if (status != HSA_STATUS_SUCCESS)
+            return status;
+        out->push_back(isaName);
+        return HSA_STATUS_SUCCESS;
+    };
+
+    status = SIBIR_HSA_CHECK(coreApi.hsa_agent_iterate_isas_fn(agent, getIsaNameCallback, &supportedAgentIsaNames));
+
+    if (status != HSA_STATUS_SUCCESS)
+        return status;
+    // Assert that there's only one supported ISA for the agent
+    assert(supportedAgentIsaNames.size() == 1);
+
+    entry.isa = supportedAgentIsaNames[0];
+
+    return HSA_STATUS_SUCCESS;
+}
+
+
+hsa_status_t Disassembler::initGpuAgentsMap() {
     int i = 0;
     auto& coreTable = SibirHsaInterceptor::Instance().getSavedHsaTables().core;
 
     auto returnGpuAgentsCallback = [](hsa_agent_t agent, void* data) {
-        auto agent_list = reinterpret_cast<std::vector<hsa_agent_t>*>(data);
+        auto agentMap = reinterpret_cast<std::unordered_map<decltype(hsa_agent_t::handle), hsa_agent_entry_t>*>(data);
         hsa_device_type_t dev_type = HSA_DEVICE_TYPE_CPU;
 
         hsa_status_t stat = hsa_agent_get_info(agent, HSA_AGENT_INFO_DEVICE, &dev_type);
 
         if (stat != HSA_STATUS_SUCCESS)
             return stat;
-        if (dev_type == HSA_DEVICE_TYPE_GPU)
-            agent_list->push_back(agent);
+        if (dev_type == HSA_DEVICE_TYPE_GPU) {
+            hsa_agent_entry_t entry;
+            Disassembler::populateAgentInfo(agent, entry);
+            agentMap->insert({agent.handle, entry});
+        }
 
         return stat;
     };
@@ -122,14 +120,13 @@ std::vector<Instr *> Disassembler::disassemble(sibir_address_t kernelObject) {
         reinterpret_cast<void*>(kernelObject), &executable));
 
     if (agents_.empty())
-        SIBIR_HSA_CHECK(initGpuAgents());
+        SIBIR_HSA_CHECK(initGpuAgentsMap());
 
     // TODO: Maybe make this part a private method instead of lambda?
-    // TODO: Cache the isa name queried in a map with their hsa_agent_ts
 
-    auto callbackData = std::make_pair( hsa_agent_t{}, kernelObject);
+    auto findKoAgentCallbackData = std::make_pair(hsa_agent_t{}, kernelObject);
 
-    auto findKoSymbolIterator = [](hsa_executable_t e, hsa_agent_t a, hsa_executable_symbol_t s, void* data){
+    auto findKoAgentIterator = [](hsa_executable_t e, hsa_agent_t a, hsa_executable_symbol_t s, void* data){
         auto cbd = reinterpret_cast<std::pair<hsa_agent_t, uint64_t>*>(data);
         uint64_t ko;
         auto& coreApi = SibirHsaInterceptor::Instance().getSavedHsaTables().core;
@@ -139,40 +136,15 @@ std::vector<Instr *> Disassembler::disassemble(sibir_address_t kernelObject) {
         return HSA_STATUS_SUCCESS;
     };
 
-    for (auto agent: agents_)
-        SIBIR_HSA_CHECK(coreApi.hsa_executable_iterate_agent_symbols_fn(executable, agent,
-                                                                        findKoSymbolIterator, &callbackData));
+    for (const auto& agent: agents_)
+        SIBIR_HSA_CHECK(coreApi.hsa_executable_iterate_agent_symbols_fn(executable, {agent.first},
+                                                                        findKoAgentIterator, &findKoAgentCallbackData));
 
-    auto symbolAgent = callbackData.first;
-    std::string agentName;
-    agentName.resize(64);
-    // Get the name (architecture) of the agent
-    coreApi.hsa_agent_get_info_fn(symbolAgent, HSA_AGENT_INFO_ISA, agentName.data());
+    hsa_agent_t symbolAgent = findKoAgentCallbackData.first;
 
-    // Get the Isa name of the agent
-    std::vector<std::string> supportedAgentIsaNames;
+    //TODO: add check here in case the symbol's agent was not found
 
-    auto getIsaNameCallback = [](hsa_isa_t isa, void* data){
-        auto out = reinterpret_cast<std::vector<std::string>*>(data);
-        auto coreApi = SibirHsaInterceptor::Instance().getSavedHsaTables().core;
-        hsa_status_t status = HSA_STATUS_ERROR;
-        uint32_t isaNameSize;
-        status = SIBIR_HSA_CHECK(coreApi.hsa_isa_get_info_alt_fn(isa, HSA_ISA_INFO_NAME_LENGTH, &isaNameSize));
-        if (status != HSA_STATUS_SUCCESS)
-            return status;
-        std::string isaName;
-        isaName.resize(isaNameSize);
-        status = SIBIR_HSA_CHECK(coreApi.hsa_isa_get_info_alt_fn(isa, HSA_ISA_INFO_NAME, isaName.data()));
-        if (status != HSA_STATUS_SUCCESS)
-            return status;
-        out->push_back(isaName);
-        return HSA_STATUS_SUCCESS;
-    };
-
-    SIBIR_HSA_CHECK(coreApi.hsa_agent_iterate_isas_fn(symbolAgent, getIsaNameCallback, &supportedAgentIsaNames));
-
-    // Assert that there's only one supported ISA for the agent
-    assert(supportedAgentIsaNames.size() == 1);
+    std::string isaName = agents_[symbolAgent.handle].isa;
 
     // Disassemble using AMD_COMGR
 
@@ -180,7 +152,7 @@ std::vector<Instr *> Disassembler::disassemble(sibir_address_t kernelObject) {
 
     amd_comgr_disassembly_info_t disassemblyInfo;
 
-    SIBIR_AMD_COMGR_CHECK(amd_comgr_create_disassembly_info(supportedAgentIsaNames[0].c_str(),
+    SIBIR_AMD_COMGR_CHECK(amd_comgr_create_disassembly_info(isaName.c_str(),
                                                             &readMemoryCallback,
                                                             &printInstructionCallback,
                                                             &printAddressCallback,
@@ -196,65 +168,6 @@ std::vector<Instr *> Disassembler::disassemble(sibir_address_t kernelObject) {
         dCallbackData.second = dCallbackData.first.find("s_endpgm") != std::string::npos;
         Addr += Size;
     }
-//
-//    if (InstructionsIdx != InstructionsLen) {
-//        fail("too few instructions\n");
-//    }
-//
-//    Addr = sizeof(Program) - 1;
-//    Size = 0;
-//    Status = amd_comgr_disassemble_instruction(DisassemblyInfo, Addr,
-//                                               (void *)&ExpectedUserData, &Size);
-//    if (Status != AMD_COMGR_STATUS_ERROR) {
-//        fail("successfully disassembled invalid instruction encoding");
-//    }
-//
-//    Status = amd_comgr_destroy_disassembly_info(DisassemblyInfo);
-//    checkError(Status, "amd_comgr_destroy_disassembly_info");
-//
-//    return EXIT_SUCCESS;
-//
-//
-//    // For now assume gfx908
-//    // TODO: add the architecture code from the dbgapi headers
-//    amd_dbgapi_architecture_id_t arch;
-//    amd_dbgapi_get_architecture(0x030, &arch);
-//
-//
-//    amd_dbgapi_size_t maxInstrLen;
-//    amd_dbgapi_architecture_get_info(arch, AMD_DBGAPI_ARCHITECTURE_INFO_LARGEST_INSTRUCTION_SIZE,
-//                                     sizeof(amd_dbgapi_size_t),
-//                                     &maxInstrLen);
-//
-//    bool is_end = false;
-//    // The decoded instruction will be malloced by ::amd_dbgapi_disassemble_instruction
-//    // It has to be copied and freed
-//    char*instChar{};
-//    auto curr_address = kernelEntryPoint;
-//    amd_dbgapi_size_t instrSize;
-//
-//    std::vector<std::pair<std::string, std::vector<std::byte>>> instList;
-//    while(!is_end) {
-//        instrSize = maxInstrLen;
-//
-//        amd_dbgapi_disassemble_instruction(arch, curr_address, &instrSize,
-//                                           reinterpret_cast<void*>(curr_address),
-//                                           &instChar, nullptr, {});
-//
-//        std::vector<std::byte> instBytes(instrSize);
-//        // Copy the instruction bytes
-//        for (amd_dbgapi_size_t i = 0; i < instrSize; i++) {
-//            instBytes[i] = reinterpret_cast<std::byte*>(curr_address)[i];
-//        }
-//        // Copy the decoded instruction string
-//        std::string instStr(instChar);
-//
-//        free(instChar);
-//        instList.emplace_back(instStr, instBytes);
-//
-//        curr_address += instrSize;
-//        is_end = instStr.find("s_endpgm") != std::string::npos;
-//    }
-//    return instList;
     return {};
 }
+
--- src/sibir/disassembler.h
+++ src/sibir/disassembler.h
@@ -2,18 +2,30 @@
 #define DISASSEMBLER_H
 #include <hsa/hsa.h>
 #include <vector>
+#include <unordered_map>
 
 #include "sibir_types.h"
 
 class Disassembler {
  private:
-    std::vector<hsa_agent_t> agents_{};
+    // Cache the agent information needed for disassembly
+    // Maybe we need more than just a single entry
+    // Maybe this is not the right place to do it
+    typedef struct hsa_agent_entry_s {
+        std::string isa;
+    } hsa_agent_entry_t;
+
+    // Use the agent handle for hashing
+    std::unordered_map<decltype(hsa_agent_t::handle), hsa_agent_entry_t> agents_{};
 
     /**
      * Initializes the GPU agents the first time a disassembly request is sent
      * @return status of the agent query from HSA
      */
-    hsa_status_t initGpuAgents();
+    hsa_status_t initGpuAgentsMap();
+
+
+    static hsa_status_t populateAgentInfo(hsa_agent_t agent, hsa_agent_entry_t& entry);
 
     Disassembler() {}
     ~Disassembler() {}
