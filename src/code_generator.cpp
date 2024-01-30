f2327a112be695da03691ffa98ff1f73340349a6
matinraayai <30674652+matinraayai@users.noreply.github.com>
2024-01-30 13:35:26 -0500

    Added an import of SmallVector to avoid undefined symbols + commented out the code loading part in CodeGen.

--- src/code_generator.cpp
+++ src/code_generator.cpp
@@ -7,7 +7,7 @@
 
 #include <memory>
 
-#include "MCTargetDesc/AMDGPUTargetStreamer.h"
+//#include "MCTargetDesc/AMDGPUTargetStreamer.h"
 #include "code_object_manager.hpp"
 #include "disassembler.hpp"
 #include "elfio/elfio.hpp"
@@ -18,6 +18,7 @@
 #include "hsa_intercept.hpp"
 #include "hsa_isa.hpp"
 #include "hsa_loaded_code_object.hpp"
+#include <llvm/ADT/SmallVector.h>
 #include "llvm/ADT/StringExtras.h"
 #include "llvm/CodeGen/MachineModuleInfo.h"
 #include "llvm/CodeGen/TargetInstrInfo.h"
@@ -418,121 +419,121 @@ void luthier::CodeGenerator::instrument(hsa::Instr &instr, const void *deviceFun
     auto storage = targetExecutable.getLoadedCodeObjects()[0].getStorageMemory();
     auto instrumentedElfView = code::ElfView::makeView(storage);
 
-    // Find the symbol that requires instrumentation.
-    std::optional<code::SymbolView> storageSymbol = instrumentedElfView->getSymbol(symbolName);
-    if (!storageSymbol.has_value())
-        throw std::runtime_error(fmt::format("Failed to find symbol {} in the copied executable", symbolName));
-    auto meta = storageSymbol->getMetaData();
-    fmt::println("Number of SGPRS: {}", meta.usedSGPRs_);
-    fmt::println("Number of VGPRS: {}", meta.usedVGPRs_);
-
-    std::unique_ptr<llvm::MCObjectFileInfo> MOFI(
-        targetInfo.getTarget()->createMCObjectFileInfo(ctx, /*PIC*/ true, /*large code model*/ false));
-    auto textSection = MOFI->getTextSection();
-    fmt::println("Does the text section have instructions? {}", textSection->hasInstructions());
-    ctx.setObjectFileInfo(MOFI.get());
-    //    ctx->setAllowTemporaryLabels(true);
-    ctx.setGenDwarfForAssembly(false);
-
-    llvm::SmallVector<char> out;
-
-    llvm::raw_svector_ostream VOS(out);
-
-    llvm::MCCodeEmitter *CE = targetInfo.getTarget()->createMCCodeEmitter(*targetInfo.getMCInstrInfo(), ctx);
-    llvm::MCAsmBackend *MAB = targetInfo.getTarget()->createMCAsmBackend(
-        *targetInfo.getMCSubTargetInfo(), *targetInfo.getMCRegisterInfo(), targetInfo.getTargetOptions().MCOptions);
-
-    auto Str = std::unique_ptr<llvm::MCStreamer>(targetInfo.getTarget()->createMCObjectStreamer(
-        llvm::Triple(agent.getIsa().getLLVMTargetTriple()), ctx, std::unique_ptr<llvm::MCAsmBackend>(MAB),
-        MAB->createObjectWriter(VOS), std::unique_ptr<llvm::MCCodeEmitter>(CE), *targetInfo.getMCSubTargetInfo(), true,
-        false,
-        /*DWARFMustBeAtTheEnd*/ false));
-
-    //    Str->initSections(false, *targetInfo.STI_);
-    Str->switchSection(ctx.getObjectFileInfo()->getTextSection());
-    Str->emitCodeAlignment(llvm::Align(ctx.getObjectFileInfo()->getTextSectionAlignment()),
-                           targetInfo.getMCSubTargetInfo());
-
-    for (const auto &inst: *targetFunction) { Str->emitInstruction(inst.getInstr(), *targetInfo.getMCSubTargetInfo()); }
-    auto kernelName = instr.getExecutableSymbol().getName();
-    //
-    auto kName = kernelName.substr(0, kernelName.find(".kd"));
-
-    auto KernelDescriptor = instr.getExecutableSymbol().getKernelDescriptor();
-    //
-    llvm::MCSymbolELF *KernelCodeSymbol = cast<llvm::MCSymbolELF>(ctx.getOrCreateSymbol(llvm::Twine(kName)));
-
-    //    Str->pushSection();
-    Str->switchSection(ctx.getObjectFileInfo()->getReadOnlySection());
-
-    Str->emitValueToAlignment(llvm::Align(64), 0, 1, 0);
-    ctx.getObjectFileInfo()->getReadOnlySection()->ensureMinAlignment(llvm::Align(64));
-
-    llvm::MCSymbolELF *KernelDescriptorSymbol = cast<llvm::MCSymbolELF>(ctx.getOrCreateSymbol(kernelName));
-
-    // Copy kernel descriptor symbol's binding, other and visibility from the
-    // kernel code symbol.
-    KernelDescriptorSymbol->setBinding(KernelCodeSymbol->getBinding());
-    KernelDescriptorSymbol->setOther(KernelCodeSymbol->getOther());
-    KernelDescriptorSymbol->setVisibility(KernelCodeSymbol->getVisibility());
-    // Kernel descriptor symbol's type and size are fixed.
-    KernelDescriptorSymbol->setType(llvm::ELF::STT_OBJECT);
-    KernelDescriptorSymbol->setSize(llvm::MCConstantExpr::create(sizeof(hsa::KernelDescriptor), ctx));
-
-    //    // The visibility of the kernel code symbol must be protected or less to allow
-    //    // static relocations from the kernel descriptor to be used.
-    if (KernelCodeSymbol->getVisibility() == llvm::ELF::STV_DEFAULT)
-        KernelCodeSymbol->setVisibility(llvm::ELF::STV_PROTECTED);
-    //
-    Str->emitLabel(KernelDescriptorSymbol);
-    Str->emitInt32(KernelDescriptor->groupSegmentFixedSize);
-    Str->emitInt32(KernelDescriptor->privateSegmentFixedSize);
-    Str->emitInt32(KernelDescriptor->kernArgSize);
-    //
-    for (uint8_t Res: KernelDescriptor->reserved0) Str->emitInt8(Res);
-
-    //    // FIXME: Remove the use of VK_AMDGPU_REL64 in the expression below. The
-    //    // expression being created is:
-    //    //   (start of kernel code) - (start of kernel descriptor)
-    //    // It implies R_AMDGPU_REL64, but ends up being R_AMDGPU_ABS64.
-    Str->emitValue(llvm::MCBinaryExpr::createSub(
-                       llvm::MCSymbolRefExpr::create(KernelCodeSymbol, llvm::MCSymbolRefExpr::VK_AMDGPU_REL64, ctx),
-                       llvm::MCSymbolRefExpr::create(KernelDescriptorSymbol, llvm::MCSymbolRefExpr::VK_None, ctx), ctx),
-                   sizeof(KernelDescriptor->kernelCodeEntryByteOffset));
-    for (uint8_t Res: KernelDescriptor->reserved1) Str->emitInt8(Res);
-    Str->emitInt32(KernelDescriptor->computePgmRsrc3);
-    Str->emitInt32(KernelDescriptor->computePgmRsrc1);
-    Str->emitInt32(KernelDescriptor->computePgmRsrc2);
-    Str->emitInt16(KernelDescriptor->kernelCodeProperties);
-    Str->emitInt16(KernelDescriptor->kernArgPreload);
-    for (uint8_t Res: KernelDescriptor->reserved2) Str->emitInt8(Res);
-
-    //    Str->popSection();
-    //    Str->switchSection(ctx.getObjectFileInfo()->getTextSection());
-
-    fmt::println("Does the text section have instructions? {}", textSection->hasInstructions());
-
-    Str->getTargetStreamer()->finish();
-    Str->finish();
-    Str->finishImpl();
-    auto finalView =
-        code::ElfView::makeView(luthier::byte_string_view(reinterpret_cast<std::byte *>(out.data()), out.size()));
-    fmt::println("Length of the out vector: {}", out.size());
-    fmt::println("Type of the code object created: {}", finalView->getElfIo().get_type());
-    fmt::println("Number of symbols: {}", finalView->getNumSymbols());
-
-    finalView->getElfIo().sections[0]->get_data();
-    //    fmt::println("Symbol name : {}", finalView->getSymbol(0)->getName());
-    //    fmt::println("Symbol name : {}", finalView->getSymbol(1)->getName());
-    //    finalView->getSymbol(0)->getData();
-    fmt::println("Data works");
-    finalView->getElfIo().sections[1]->get_data();
-
-    //    auto executable = compileRelocatableToExecutable({reinterpret_cast<std::byte*>(out.data()), out.size()}, instr.getAgent());
-    //    delete Str;
-    //    Str->finishImpl();
-
-    //    targetStr->finish();
-
-    CodeObjectManager::instance().loadInstrumentedKernel(luthier::byte_string_t(storage), instr.getExecutableSymbol());
+//    // Find the symbol that requires instrumentation.
+//    std::optional<code::SymbolView> storageSymbol = instrumentedElfView->getSymbol(symbolName);
+//    if (!storageSymbol.has_value())
+//        throw std::runtime_error(fmt::format("Failed to find symbol {} in the copied executable", symbolName));
+//    auto meta = storageSymbol->getMetaData();
+//    fmt::println("Number of SGPRS: {}", meta.usedSGPRs_);
+//    fmt::println("Number of VGPRS: {}", meta.usedVGPRs_);
+//
+//    std::unique_ptr<llvm::MCObjectFileInfo> MOFI(
+//        targetInfo.getTarget()->createMCObjectFileInfo(ctx, /*PIC*/ true, /*large code model*/ false));
+//    auto textSection = MOFI->getTextSection();
+//    fmt::println("Does the text section have instructions? {}", textSection->hasInstructions());
+//    ctx.setObjectFileInfo(MOFI.get());
+//    //    ctx->setAllowTemporaryLabels(true);
+//    ctx.setGenDwarfForAssembly(false);
+//
+//    llvm::SmallVector<char> out;
+//
+//    llvm::raw_svector_ostream VOS(out);
+//
+//    llvm::MCCodeEmitter *CE = targetInfo.getTarget()->createMCCodeEmitter(*targetInfo.getMCInstrInfo(), ctx);
+//    llvm::MCAsmBackend *MAB = targetInfo.getTarget()->createMCAsmBackend(
+//        *targetInfo.getMCSubTargetInfo(), *targetInfo.getMCRegisterInfo(), targetInfo.getTargetOptions().MCOptions);
+//
+//    auto Str = std::unique_ptr<llvm::MCStreamer>(targetInfo.getTarget()->createMCObjectStreamer(
+//        llvm::Triple(agent.getIsa().getLLVMTargetTriple()), ctx, std::unique_ptr<llvm::MCAsmBackend>(MAB),
+//        MAB->createObjectWriter(VOS), std::unique_ptr<llvm::MCCodeEmitter>(CE), *targetInfo.getMCSubTargetInfo(), true,
+//        false,
+//        /*DWARFMustBeAtTheEnd*/ false));
+//
+//    //    Str->initSections(false, *targetInfo.STI_);
+//    Str->switchSection(ctx.getObjectFileInfo()->getTextSection());
+//    Str->emitCodeAlignment(llvm::Align(ctx.getObjectFileInfo()->getTextSectionAlignment()),
+//                           targetInfo.getMCSubTargetInfo());
+//
+//    for (const auto &inst: *targetFunction) { Str->emitInstruction(inst.getInstr(), *targetInfo.getMCSubTargetInfo()); }
+//    auto kernelName = instr.getExecutableSymbol().getName();
+//    //
+//    auto kName = kernelName.substr(0, kernelName.find(".kd"));
+//
+//    auto KernelDescriptor = instr.getExecutableSymbol().getKernelDescriptor();
+//    //
+//    llvm::MCSymbolELF *KernelCodeSymbol = cast<llvm::MCSymbolELF>(ctx.getOrCreateSymbol(llvm::Twine(kName)));
+//
+//    //    Str->pushSection();
+//    Str->switchSection(ctx.getObjectFileInfo()->getReadOnlySection());
+//
+//    Str->emitValueToAlignment(llvm::Align(64), 0, 1, 0);
+//    ctx.getObjectFileInfo()->getReadOnlySection()->ensureMinAlignment(llvm::Align(64));
+//
+//    llvm::MCSymbolELF *KernelDescriptorSymbol = cast<llvm::MCSymbolELF>(ctx.getOrCreateSymbol(kernelName));
+//
+//    // Copy kernel descriptor symbol's binding, other and visibility from the
+//    // kernel code symbol.
+//    KernelDescriptorSymbol->setBinding(KernelCodeSymbol->getBinding());
+//    KernelDescriptorSymbol->setOther(KernelCodeSymbol->getOther());
+//    KernelDescriptorSymbol->setVisibility(KernelCodeSymbol->getVisibility());
+//    // Kernel descriptor symbol's type and size are fixed.
+//    KernelDescriptorSymbol->setType(llvm::ELF::STT_OBJECT);
+//    KernelDescriptorSymbol->setSize(llvm::MCConstantExpr::create(sizeof(hsa::KernelDescriptor), ctx));
+//
+//    //    // The visibility of the kernel code symbol must be protected or less to allow
+//    //    // static relocations from the kernel descriptor to be used.
+//    if (KernelCodeSymbol->getVisibility() == llvm::ELF::STV_DEFAULT)
+//        KernelCodeSymbol->setVisibility(llvm::ELF::STV_PROTECTED);
+//    //
+//    Str->emitLabel(KernelDescriptorSymbol);
+//    Str->emitInt32(KernelDescriptor->groupSegmentFixedSize);
+//    Str->emitInt32(KernelDescriptor->privateSegmentFixedSize);
+//    Str->emitInt32(KernelDescriptor->kernArgSize);
+//    //
+//    for (uint8_t Res: KernelDescriptor->reserved0) Str->emitInt8(Res);
+//
+//    //    // FIXME: Remove the use of VK_AMDGPU_REL64 in the expression below. The
+//    //    // expression being created is:
+//    //    //   (start of kernel code) - (start of kernel descriptor)
+//    //    // It implies R_AMDGPU_REL64, but ends up being R_AMDGPU_ABS64.
+//    Str->emitValue(llvm::MCBinaryExpr::createSub(
+//                       llvm::MCSymbolRefExpr::create(KernelCodeSymbol, llvm::MCSymbolRefExpr::VK_AMDGPU_REL64, ctx),
+//                       llvm::MCSymbolRefExpr::create(KernelDescriptorSymbol, llvm::MCSymbolRefExpr::VK_None, ctx), ctx),
+//                   sizeof(KernelDescriptor->kernelCodeEntryByteOffset));
+//    for (uint8_t Res: KernelDescriptor->reserved1) Str->emitInt8(Res);
+//    Str->emitInt32(KernelDescriptor->computePgmRsrc3);
+//    Str->emitInt32(KernelDescriptor->computePgmRsrc1);
+//    Str->emitInt32(KernelDescriptor->computePgmRsrc2);
+//    Str->emitInt16(KernelDescriptor->kernelCodeProperties);
+//    Str->emitInt16(KernelDescriptor->kernArgPreload);
+//    for (uint8_t Res: KernelDescriptor->reserved2) Str->emitInt8(Res);
+//
+//    //    Str->popSection();
+//    //    Str->switchSection(ctx.getObjectFileInfo()->getTextSection());
+//
+//    fmt::println("Does the text section have instructions? {}", textSection->hasInstructions());
+//
+//    Str->getTargetStreamer()->finish();
+//    Str->finish();
+//    Str->finishImpl();
+//    auto finalView =
+//        code::ElfView::makeView(luthier::byte_string_view(reinterpret_cast<std::byte *>(out.data()), out.size()));
+//    fmt::println("Length of the out vector: {}", out.size());
+//    fmt::println("Type of the code object created: {}", finalView->getElfIo().get_type());
+//    fmt::println("Number of symbols: {}", finalView->getNumSymbols());
+//
+//    finalView->getElfIo().sections[0]->get_data();
+//    //    fmt::println("Symbol name : {}", finalView->getSymbol(0)->getName());
+//    //    fmt::println("Symbol name : {}", finalView->getSymbol(1)->getName());
+//    //    finalView->getSymbol(0)->getData();
+//    fmt::println("Data works");
+//    finalView->getElfIo().sections[1]->get_data();
+//
+//    //    auto executable = compileRelocatableToExecutable({reinterpret_cast<std::byte*>(out.data()), out.size()}, instr.getAgent());
+//    //    delete Str;
+//    //    Str->finishImpl();
+//
+//    //    targetStr->finish();
+//
+//    CodeObjectManager::instance().loadInstrumentedKernel(luthier::byte_string_t(storage), instr.getExecutableSymbol());
 }
