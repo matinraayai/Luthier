d91985ce8b8f3ca9017690c1918d0a8688fb0a74
matinraayai <30674652+matinraayai@users.noreply.github.com>
2024-02-19 13:55:04 -0500

    Updated hsa_loaded_code_object.*pp to not use byte string.

--- src/hsa_code_object_reader.cpp
+++ src/hsa_code_object_reader.cpp
@@ -1,16 +1,19 @@
 #include "hsa_code_object_reader.hpp"
 
+#include <llvm/ADT/StringExtras.h>
+
 #include "hsa_intercept.hpp"
 
-luthier::hsa::CodeObjectReader luthier::hsa::CodeObjectReader::createFromMemory(luthier::byte_string_view elf) {
+luthier::hsa::CodeObjectReader luthier::hsa::CodeObjectReader::createFromMemory(llvm::StringRef elf) {
     const auto& coreTable = HsaInterceptor::instance().getSavedHsaTables().core;
     hsa_code_object_reader_t reader;
     LUTHIER_HSA_CHECK(coreTable.hsa_code_object_reader_create_from_memory_fn(elf.data(), elf.size(), &reader));
     return CodeObjectReader{reader};
 }
-luthier::hsa::CodeObjectReader luthier::hsa::CodeObjectReader::createFromMemory(const luthier::byte_string_t& elf) {
-    return createFromMemory(luthier::byte_string_view(elf));
+luthier::hsa::CodeObjectReader luthier::hsa::CodeObjectReader::createFromMemory(llvm::ArrayRef<uint8_t> elf) {
+    return createFromMemory(llvm::toStringRef(elf));
 }
+
 void luthier::hsa::CodeObjectReader::destroy() {
     LUTHIER_HSA_CHECK(getApiTable().core.hsa_code_object_reader_destroy_fn(asHsaType()));
 }
