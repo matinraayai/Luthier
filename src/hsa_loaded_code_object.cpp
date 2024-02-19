dfdc8ddd3b3db1eb80cc1085a3551ca8b8e6e2eb
matinraayai <30674652+matinraayai@users.noreply.github.com>
2024-02-19 13:57:18 -0500

    Updated hsa_loaded_code_object.*pp to not use byte_string.

--- src/hsa_loaded_code_object.cpp
+++ src/hsa_loaded_code_object.cpp
@@ -1,7 +1,8 @@
 #include "hsa_loaded_code_object.hpp"
+
+#include "hsa_agent.hpp"
 #include "hsa_executable.hpp"
 #include "hsa_intercept.hpp"
-#include "hsa_agent.hpp"
 
 namespace luthier::hsa {
 
@@ -10,85 +11,71 @@ LoadedCodeObject::LoadedCodeObject(hsa_loaded_code_object_t lco) : HandleType<hs
 Executable LoadedCodeObject::getExecutable() const {
     hsa_executable_t exec;
     LUTHIER_HSA_CHECK(getLoaderTable().hsa_ven_amd_loader_loaded_code_object_get_info(
-        this->asHsaType(),
-        HSA_VEN_AMD_LOADER_LOADED_CODE_OBJECT_INFO_EXECUTABLE,
-        &exec));
+        this->asHsaType(), HSA_VEN_AMD_LOADER_LOADED_CODE_OBJECT_INFO_EXECUTABLE, &exec));
     return Executable(exec);
 }
 
 GpuAgent LoadedCodeObject::getAgent() const {
     hsa_agent_t agent;
     LUTHIER_HSA_CHECK(getLoaderTable().hsa_ven_amd_loader_loaded_code_object_get_info(
-        this->asHsaType(),
-        HSA_VEN_AMD_LOADER_LOADED_CODE_OBJECT_INFO_AGENT,
-        &agent));
+        this->asHsaType(), HSA_VEN_AMD_LOADER_LOADED_CODE_OBJECT_INFO_AGENT, &agent));
     return GpuAgent(agent);
 }
 
 hsa_ven_amd_loader_code_object_storage_type_t LoadedCodeObject::getStorageType() const {
     hsa_ven_amd_loader_code_object_storage_type_t storageType;
     LUTHIER_HSA_CHECK(getLoaderTable().hsa_ven_amd_loader_loaded_code_object_get_info(
-        this->asHsaType(),
-        HSA_VEN_AMD_LOADER_LOADED_CODE_OBJECT_INFO_CODE_OBJECT_STORAGE_TYPE,
-        &storageType));
+        this->asHsaType(), HSA_VEN_AMD_LOADER_LOADED_CODE_OBJECT_INFO_CODE_OBJECT_STORAGE_TYPE, &storageType));
     return storageType;
 }
 
-luthier::byte_string_view LoadedCodeObject::getStorageMemory() const {
+llvm::ArrayRef<uint8_t> LoadedCodeObject::getStorageMemory() const {
     luthier_address_t storageBase;
-    LUTHIER_HSA_CHECK(getLoaderTable().hsa_ven_amd_loader_loaded_code_object_get_info(this->asHsaType(),
-                                                                                      HSA_VEN_AMD_LOADER_LOADED_CODE_OBJECT_INFO_CODE_OBJECT_STORAGE_MEMORY_BASE,
-                                                                                      &storageBase));
+    LUTHIER_HSA_CHECK(getLoaderTable().hsa_ven_amd_loader_loaded_code_object_get_info(
+        this->asHsaType(), HSA_VEN_AMD_LOADER_LOADED_CODE_OBJECT_INFO_CODE_OBJECT_STORAGE_MEMORY_BASE, &storageBase));
 
     uint64_t storageSize;
-    getLoaderTable().hsa_ven_amd_loader_loaded_code_object_get_info(this->asHsaType(),
-                                                                    HSA_VEN_AMD_LOADER_LOADED_CODE_OBJECT_INFO_CODE_OBJECT_STORAGE_MEMORY_SIZE,
-                                                                    &storageSize);
+    getLoaderTable().hsa_ven_amd_loader_loaded_code_object_get_info(
+        this->asHsaType(), HSA_VEN_AMD_LOADER_LOADED_CODE_OBJECT_INFO_CODE_OBJECT_STORAGE_MEMORY_SIZE, &storageSize);
 
-    return {reinterpret_cast<std::byte *>(storageBase), storageSize};
+    return {reinterpret_cast<uint8_t *>(storageBase), storageSize};
 }
 
 int LoadedCodeObject::getStorageFile() const {
     int fd;
-    LUTHIER_HSA_CHECK(getLoaderTable().hsa_ven_amd_loader_loaded_code_object_get_info(this->asHsaType(),
-                                                                                      HSA_VEN_AMD_LOADER_LOADED_CODE_OBJECT_INFO_CODE_OBJECT_STORAGE_FILE,
-                                                                                      &fd));
+    LUTHIER_HSA_CHECK(getLoaderTable().hsa_ven_amd_loader_loaded_code_object_get_info(
+        this->asHsaType(), HSA_VEN_AMD_LOADER_LOADED_CODE_OBJECT_INFO_CODE_OBJECT_STORAGE_FILE, &fd));
     return fd;
 }
 
 long LoadedCodeObject::getLoadDelta() const {
     long loadDelta;
-    LUTHIER_HSA_CHECK(getLoaderTable().hsa_ven_amd_loader_loaded_code_object_get_info(this->asHsaType(),
-                                                                                      HSA_VEN_AMD_LOADER_LOADED_CODE_OBJECT_INFO_LOAD_DELTA,
-                                                                                      &loadDelta));
+    LUTHIER_HSA_CHECK(getLoaderTable().hsa_ven_amd_loader_loaded_code_object_get_info(
+        this->asHsaType(), HSA_VEN_AMD_LOADER_LOADED_CODE_OBJECT_INFO_LOAD_DELTA, &loadDelta));
     return loadDelta;
 }
 
-luthier::byte_string_view LoadedCodeObject::getLoadedMemory() const {
+llvm::ArrayRef<uint8_t> LoadedCodeObject::getLoadedMemory() const {
     luthier_address_t loadBase;
-    LUTHIER_HSA_CHECK(getLoaderTable().hsa_ven_amd_loader_loaded_code_object_get_info(this->asHsaType(),
-                                                                                      HSA_VEN_AMD_LOADER_LOADED_CODE_OBJECT_INFO_LOAD_BASE,
-                                                                                      &loadBase));
+    LUTHIER_HSA_CHECK(getLoaderTable().hsa_ven_amd_loader_loaded_code_object_get_info(
+        this->asHsaType(), HSA_VEN_AMD_LOADER_LOADED_CODE_OBJECT_INFO_LOAD_BASE, &loadBase));
 
     uint64_t loadSize;
-    getLoaderTable().hsa_ven_amd_loader_loaded_code_object_get_info(this->asHsaType(),
-                                                                    HSA_VEN_AMD_LOADER_LOADED_CODE_OBJECT_INFO_LOAD_SIZE,
-                                                                    &loadSize);
+    getLoaderTable().hsa_ven_amd_loader_loaded_code_object_get_info(
+        this->asHsaType(), HSA_VEN_AMD_LOADER_LOADED_CODE_OBJECT_INFO_LOAD_SIZE, &loadSize);
 
-    return {reinterpret_cast<std::byte *>(loadBase), loadSize};
+    return {reinterpret_cast<uint8_t *>(loadBase), loadSize};
 }
 
 std::string LoadedCodeObject::getUri() const {
     unsigned int uriLength;
-    LUTHIER_HSA_CHECK(getLoaderTable().hsa_ven_amd_loader_loaded_code_object_get_info(this->asHsaType(),
-                                                                                      HSA_VEN_AMD_LOADER_LOADED_CODE_OBJECT_INFO_URI_LENGTH,
-                                                                                      &uriLength));
+    LUTHIER_HSA_CHECK(getLoaderTable().hsa_ven_amd_loader_loaded_code_object_get_info(
+        this->asHsaType(), HSA_VEN_AMD_LOADER_LOADED_CODE_OBJECT_INFO_URI_LENGTH, &uriLength));
 
     std::string uri;
     uri.resize(uriLength);
-    getLoaderTable().hsa_ven_amd_loader_loaded_code_object_get_info(this->asHsaType(),
-                                                                    HSA_VEN_AMD_LOADER_LOADED_CODE_OBJECT_INFO_URI,
-                                                                    uri.data());
+    getLoaderTable().hsa_ven_amd_loader_loaded_code_object_get_info(
+        this->asHsaType(), HSA_VEN_AMD_LOADER_LOADED_CODE_OBJECT_INFO_URI, uri.data());
 
     return uri;
 }
