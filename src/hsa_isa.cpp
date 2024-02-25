462428788f52ef82bbc75429f55e6ced44387150
matinraayai <30674652+matinraayai@users.noreply.github.com>
2024-02-25 01:58:31 -0500

    Switched to new LLVM HSA errors.

--- src/hsa_isa.cpp
+++ src/hsa_isa.cpp
@@ -6,101 +6,131 @@
 
 namespace luthier::hsa {
 
-Isa Isa::fromName(const char *isaName) {
-    hsa_isa_t isa;
-    const auto &coreApi = luthier::HsaInterceptor::instance().getSavedHsaTables().core;
-    LUTHIER_HSA_CHECK(coreApi.hsa_isa_from_name_fn(isaName, &isa));
-    return Isa(isa);
+llvm::Expected<ISA> ISA::fromName(const char *isaName) {
+  hsa_isa_t Isa;
+  const auto &coreApi =
+      luthier::HsaInterceptor::instance().getSavedHsaTables().core;
+  LUTHIER_RETURN_ON_ERROR(
+      LUTHIER_HSA_SUCCESS_CHECK(coreApi.hsa_isa_from_name_fn(isaName, &Isa)));
+
+  return luthier::hsa::ISA(Isa);
 }
-std::string Isa::getName() const {
-    uint32_t isaNameSize;
-    LUTHIER_HSA_CHECK(
-        getApiTable().core.hsa_isa_get_info_alt_fn(this->asHsaType(), HSA_ISA_INFO_NAME_LENGTH, &isaNameSize));
-    std::string isaName(isaNameSize - 1, '\0');
-    LUTHIER_HSA_CHECK(
-        getApiTable().core.hsa_isa_get_info_alt_fn(this->asHsaType(), HSA_ISA_INFO_NAME, &isaName.front()));
-    return isaName;
+llvm::Expected<std::string> ISA::getName() const {
+  uint32_t IsaNameSize;
+  LUTHIER_RETURN_ON_ERROR(
+      LUTHIER_HSA_SUCCESS_CHECK(getApiTable().core.hsa_isa_get_info_alt_fn(
+          this->asHsaType(), HSA_ISA_INFO_NAME_LENGTH, &IsaNameSize)));
+  std::string isaName(IsaNameSize - 1, '\0');
+  LUTHIER_RETURN_ON_ERROR(
+      LUTHIER_HSA_SUCCESS_CHECK(getApiTable().core.hsa_isa_get_info_alt_fn(
+          this->asHsaType(), HSA_ISA_INFO_NAME, &isaName.front())));
+  return isaName;
 }
 
-inline void parseIsaName(llvm::StringRef isaName, llvm::SmallVectorImpl<llvm::StringRef> &out) {
-    isaName.split(out, '-', 4);
-    LUTHIER_CHECK((out.size() == 5));
+inline void parseIsaName(llvm::StringRef isaName,
+                         llvm::SmallVectorImpl<llvm::StringRef> &out) {
+  isaName.split(out, '-', 4);
+  LUTHIER_CHECK((out.size() == 5));
 }
 
-std::string Isa::getArchitecture() const {
-    std::string isaName = getName();
-    llvm::SmallVector<llvm::StringRef> isaNameComponents;
-    parseIsaName(isaName, isaNameComponents);
-    return isaNameComponents[0].str();
-}
-std::string Isa::getVendor() const {
-    std::string isaName = getName();
-    llvm::SmallVector<llvm::StringRef> isaNameComponents;
-    parseIsaName(isaName, isaNameComponents);
-    return isaNameComponents[1].str();
-}
-std::string Isa::getOS() const {
-    std::string isaName = getName();
-    llvm::SmallVector<llvm::StringRef> isaNameComponents;
-    parseIsaName(isaName, isaNameComponents);
-    return std::move(isaNameComponents[2].str());
-}
-std::string Isa::getEnvironment() const {
-    std::string isaName = getName();
-    llvm::SmallVector<llvm::StringRef> isaNameComponents;
-    parseIsaName(isaName, isaNameComponents);
-    return isaNameComponents[3].str();
+llvm::Expected<std::string> ISA::getArchitecture() const {
+  auto IsaNameOrError = getName();
+  LUTHIER_RETURN_ON_ERROR(IsaNameOrError.takeError());
+  llvm::SmallVector<llvm::StringRef> IsaNameComponents;
+  parseIsaName(*IsaNameOrError, IsaNameComponents);
+  return IsaNameComponents[0].str();
 }
 
-std::string Isa::getProcessor() const {
-    std::string isaName = getName();
-    llvm::SmallVector<llvm::StringRef> isaNameComponents;
-    parseIsaName(isaName, isaNameComponents);
-
-    llvm::SmallVector<llvm::StringRef> features;
-    features.clear();
-    isaNameComponents[4].split(features, ':');
-
-    return features[0].str();
-}
-bool Isa::isXnacSupported() const {
-    std::string isaName = getName();
-    auto sRamECC = isaName.find("xnack");
-    if (sRamECC == std::string::npos) return false;
-    else { return isaName[sRamECC + strlen("xnack")] == '+'; }
-}
-bool Isa::isSRamECCSupported() const {
-    std::string isaName = getName();
-    auto sRamECC = isaName.find("sramecc");
-    if (sRamECC == std::string::npos) return false;
-    else { return isaName[sRamECC + strlen("sramecc")] == '+'; }
-}
-std::string Isa::getLLVMTarget() const {
-    std::string isaName = getName();
-    return isaName.substr(0, isaName.find_first_of(':'));
+llvm::Expected<std::string> ISA::getVendor() const {
+  auto IsaNameOrError = getName();
+  LUTHIER_RETURN_ON_ERROR(IsaNameOrError.takeError());
+  llvm::SmallVector<llvm::StringRef> IsaNameComponents;
+  parseIsaName(*IsaNameOrError, IsaNameComponents);
+  return IsaNameComponents[1].str();
 }
 
-std::string Isa::getLLVMTargetTriple() const {
-    std::string isaName = getName();
-    llvm::SmallVector<llvm::StringRef> isaNameComponents;
-    parseIsaName(isaName, isaNameComponents);
-    return (llvm::Twine(isaNameComponents[0]) + "-" + llvm::Twine(isaNameComponents[1]) + "-"
-            + llvm::Twine(isaNameComponents[2]) + "-" + llvm::Twine(isaNameComponents[3]))
-        .str();
+llvm::Expected<std::string> ISA::getOS() const {
+  auto IsaNameOrError = getName();
+  LUTHIER_RETURN_ON_ERROR(IsaNameOrError.takeError());
+  llvm::SmallVector<llvm::StringRef> isaNameComponents;
+  parseIsaName(*IsaNameOrError, isaNameComponents);
+  return std::move(isaNameComponents[2].str());
+}
+llvm::Expected<std::string> ISA::getEnvironment() const {
+  auto IsaNameOrError = getName();
+  LUTHIER_RETURN_ON_ERROR(IsaNameOrError.takeError());
+  llvm::SmallVector<llvm::StringRef> isaNameComponents;
+  parseIsaName(*IsaNameOrError, isaNameComponents);
+  return isaNameComponents[3].str();
 }
 
-std::string Isa::getFeatureString() const {
-    std::string isaName = getName();
-    llvm::SmallVector<llvm::StringRef> features;
-    llvm::StringRef(isaName).substr(isaName.find_first_of(':')).split(features, ":");
-    // The +/- must be before the feature code for LLVM, not after
-    std::vector<std::string> featuresOut;
-    for (auto &feat: features) {
-        auto featureToggle = feat.substr(feat.size() - 1);
-        auto featureName = feat.substr(0, feat.size() - 1);
-        featuresOut.push_back((featureToggle + featureName).str());
-    }
-    return llvm::join(featuresOut, ",");
+llvm::Expected<std::string> ISA::getProcessor() const {
+  auto IsaNameOrError = getName();
+  LUTHIER_RETURN_ON_ERROR(IsaNameOrError.takeError());
+  llvm::SmallVector<llvm::StringRef> isaNameComponents;
+  parseIsaName(*IsaNameOrError, isaNameComponents);
+
+  llvm::SmallVector<llvm::StringRef> features;
+  features.clear();
+  isaNameComponents[4].split(features, ':');
+
+  return features[0].str();
+}
+llvm::Expected<bool> ISA::isXNACSupported() const {
+  auto IsaNameOrError = getName();
+  LUTHIER_RETURN_ON_ERROR(IsaNameOrError.takeError());
+  auto sRamECC = IsaNameOrError->find("xnack");
+  if (sRamECC == std::string::npos)
+    return false;
+  else {
+    return (*IsaNameOrError)[sRamECC + strlen("xnack")] == '+';
+  }
 }
 
-}// namespace luthier::hsa
+llvm::Expected<bool> ISA::isSRAMECCSupported() const {
+  auto IsaNameOrError = getName();
+  LUTHIER_RETURN_ON_ERROR(IsaNameOrError.takeError());
+  auto sRamECC = IsaNameOrError->find("sramecc");
+  if (sRamECC == std::string::npos)
+    return false;
+  else {
+    return (*IsaNameOrError)[sRamECC + strlen("sramecc")] == '+';
+  }
+}
+
+llvm::Expected<std::string> ISA::getLLVMTarget() const {
+  auto IsaNameOrError = getName();
+  LUTHIER_RETURN_ON_ERROR(IsaNameOrError.takeError());
+  return IsaNameOrError->substr(0, IsaNameOrError->find_first_of(':'));
+}
+
+llvm::Expected<std::string> ISA::getLLVMTargetTriple() const {
+  auto IsaNameOrError = getName();
+  LUTHIER_RETURN_ON_ERROR(IsaNameOrError.takeError());
+  llvm::SmallVector<llvm::StringRef> isaNameComponents;
+  parseIsaName(*IsaNameOrError, isaNameComponents);
+  return (llvm::Twine(isaNameComponents[0]) + "-" +
+          llvm::Twine(isaNameComponents[1]) + "-" +
+          llvm::Twine(isaNameComponents[2]) + "-" +
+          llvm::Twine(isaNameComponents[3]))
+      .str();
+}
+
+llvm::Expected<std::string> ISA::getFeatureString() const {
+  auto IsaNameOrError = getName();
+  LUTHIER_RETURN_ON_ERROR(IsaNameOrError.takeError());
+  llvm::SmallVector<llvm::StringRef> features;
+  llvm::StringRef(*IsaNameOrError)
+      .substr(IsaNameOrError->find_first_of(':'))
+      .split(features, ":");
+  // The +/- must be before the feature code for LLVM, not after
+  std::vector<std::string> featuresOut;
+  for (auto &feat : features) {
+    auto featureToggle = feat.substr(feat.size() - 1);
+    auto featureName = feat.substr(0, feat.size() - 1);
+    featuresOut.push_back((featureToggle + featureName).str());
+  }
+  return llvm::join(featuresOut, ",");
+}
+
+} // namespace luthier::hsa
--- src/hsa_isa.hpp
+++ src/hsa_isa.hpp
@@ -1,91 +1,91 @@
 #ifndef HSA_ISA_HPP
 #define HSA_ISA_HPP
 #include "hsa_handle_type.hpp"
+#include <llvm/Support/Error.h>
 
 namespace luthier::hsa {
 
-class Isa : public HandleType<hsa_isa_t> {
- public:
-    explicit Isa(hsa_isa_t isa) : HandleType<hsa_isa_t>(isa){};
+class ISA : public HandleType<hsa_isa_t> {
+public:
+  explicit ISA(hsa_isa_t Isa) : HandleType<hsa_isa_t>(Isa){};
 
-    static Isa fromName(const char *isaName);
+  static llvm::Expected<ISA> fromName(const char *IsaName);
 
-    [[nodiscard]] std::string getName() const;
+  [[nodiscard]] llvm::Expected<std::string> getName() const;
 
-    [[nodiscard]] std::string getArchitecture() const;
+  [[nodiscard]] llvm::Expected<std::string> getArchitecture() const;
 
-    [[nodiscard]] std::string getVendor() const;
+  [[nodiscard]] llvm::Expected<std::string> getVendor() const;
 
-    [[nodiscard]] std::string getOS() const;
+  [[nodiscard]] llvm::Expected<std::string> getOS() const;
 
-    [[nodiscard]] std::string getEnvironment() const;
+  [[nodiscard]] llvm::Expected<std::string> getEnvironment() const;
 
-    [[nodiscard]] std::string getProcessor() const;
+  [[nodiscard]] llvm::Expected<std::string> getProcessor() const;
 
-    [[nodiscard]] bool isXnacSupported() const;
+  [[nodiscard]] llvm::Expected<bool> isXNACSupported() const;
 
-    [[nodiscard]] bool isSRamECCSupported() const;
+  [[nodiscard]] llvm::Expected<bool> isSRAMECCSupported() const;
 
-    [[nodiscard]] std::string getLLVMTarget() const;
+  [[nodiscard]] llvm::Expected<std::string> getLLVMTarget() const;
 
-    [[nodiscard]] std::string getLLVMTargetTriple() const;
+  [[nodiscard]] llvm::Expected<std::string> getLLVMTargetTriple() const;
 
-    [[nodiscard]] std::string getFeatureString() const;
+  [[nodiscard]] llvm::Expected<std::string> getFeatureString() const;
 };
 
-}// namespace luthier::hsa
+} // namespace luthier::hsa
 
 namespace std {
 
-template<>
-struct hash<luthier::hsa::Isa> {
-    size_t operator()(const luthier::hsa::Isa &obj) const {
-        return hash<unsigned long>()(obj.hsaHandle());
-    }
+template <> struct hash<luthier::hsa::ISA> {
+  size_t operator()(const luthier::hsa::ISA &obj) const {
+    return hash<unsigned long>()(obj.hsaHandle());
+  }
 };
 
-template<>
-struct less<luthier::hsa::Isa> {
-    bool operator()(const luthier::hsa::Isa &lhs, const luthier::hsa::Isa &rhs) const {
-        return lhs.hsaHandle() < rhs.hsaHandle();
-    }
+template <> struct less<luthier::hsa::ISA> {
+  bool operator()(const luthier::hsa::ISA &lhs,
+                  const luthier::hsa::ISA &rhs) const {
+    return lhs.hsaHandle() < rhs.hsaHandle();
+  }
 };
 
-template<>
-struct less_equal<luthier::hsa::Isa> {
-    bool operator()(const luthier::hsa::Isa &lhs, const luthier::hsa::Isa &rhs) const {
-        return lhs.hsaHandle() <= rhs.hsaHandle();
-    }
+template <> struct less_equal<luthier::hsa::ISA> {
+  bool operator()(const luthier::hsa::ISA &lhs,
+                  const luthier::hsa::ISA &rhs) const {
+    return lhs.hsaHandle() <= rhs.hsaHandle();
+  }
 };
 
-template<>
-struct equal_to<luthier::hsa::Isa> {
-    bool operator()(const luthier::hsa::Isa &lhs, const luthier::hsa::Isa &rhs) const {
-        return lhs.hsaHandle() == rhs.hsaHandle();
-    }
+template <> struct equal_to<luthier::hsa::ISA> {
+  bool operator()(const luthier::hsa::ISA &Lhs,
+                  const luthier::hsa::ISA &Rhs) const {
+    return Lhs.hsaHandle() == Rhs.hsaHandle();
+  }
 };
 
-template<>
-struct not_equal_to<luthier::hsa::Isa> {
-    bool operator()(const luthier::hsa::Isa &lhs, const luthier::hsa::Isa &rhs) const {
-        return lhs.hsaHandle() != rhs.hsaHandle();
-    }
+template <> struct not_equal_to<luthier::hsa::ISA> {
+  bool operator()(const luthier::hsa::ISA &lhs,
+                  const luthier::hsa::ISA &rhs) const {
+    return lhs.hsaHandle() != rhs.hsaHandle();
+  }
 };
 
-template<>
-struct greater<luthier::hsa::Isa> {
-    bool operator()(const luthier::hsa::Isa &lhs, const luthier::hsa::Isa &rhs) const {
-        return lhs.hsaHandle() > rhs.hsaHandle();
-    }
+template <> struct greater<luthier::hsa::ISA> {
+  bool operator()(const luthier::hsa::ISA &lhs,
+                  const luthier::hsa::ISA &rhs) const {
+    return lhs.hsaHandle() > rhs.hsaHandle();
+  }
 };
 
-template<>
-struct greater_equal<luthier::hsa::Isa> {
-    bool operator()(const luthier::hsa::Isa &lhs, const luthier::hsa::Isa &rhs) const {
-        return lhs.hsaHandle() >= rhs.hsaHandle();
-    }
+template <> struct greater_equal<luthier::hsa::ISA> {
+  bool operator()(const luthier::hsa::ISA &lhs,
+                  const luthier::hsa::ISA &rhs) const {
+    return lhs.hsaHandle() >= rhs.hsaHandle();
+  }
 };
 
-}// namespace std
+} // namespace std
 
 #endif
