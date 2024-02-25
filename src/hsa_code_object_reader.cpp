6c860e2fc8e510868ea8e06a67cfdf788da6ac5b
matinraayai <30674652+matinraayai@users.noreply.github.com>
2024-02-25 02:59:03 -0500

    Error checking for hsa_code_object_reader.*pp.

--- src/hsa_code_object_reader.cpp
+++ src/hsa_code_object_reader.cpp
@@ -2,18 +2,28 @@
 
 #include <llvm/ADT/StringExtras.h>
 
+#include "error.hpp"
 #include "hsa_intercept.hpp"
 
-luthier::hsa::CodeObjectReader luthier::hsa::CodeObjectReader::createFromMemory(llvm::StringRef elf) {
-    const auto& coreTable = HsaInterceptor::instance().getSavedHsaTables().core;
-    hsa_code_object_reader_t reader;
-    LUTHIER_HSA_CHECK(coreTable.hsa_code_object_reader_create_from_memory_fn(elf.data(), elf.size(), &reader));
-    return CodeObjectReader{reader};
+namespace luthier::hsa {
+
+llvm::Expected<CodeObjectReader>
+CodeObjectReader::createFromMemory(llvm::StringRef elf) {
+  const auto &coreTable = HsaInterceptor::instance().getSavedHsaTables().core;
+  hsa_code_object_reader_t reader;
+  LUTHIER_RETURN_ON_ERROR(LUTHIER_HSA_SUCCESS_CHECK(
+      coreTable.hsa_code_object_reader_create_from_memory_fn(
+          elf.data(), elf.size(), &reader)));
+  return CodeObjectReader{reader};
 }
-luthier::hsa::CodeObjectReader luthier::hsa::CodeObjectReader::createFromMemory(llvm::ArrayRef<uint8_t> elf) {
-    return createFromMemory(llvm::toStringRef(elf));
+llvm::Expected<CodeObjectReader>
+CodeObjectReader::createFromMemory(llvm::ArrayRef<uint8_t> elf) {
+  return createFromMemory(llvm::toStringRef(elf));
 }
 
-void luthier::hsa::CodeObjectReader::destroy() {
-    LUTHIER_HSA_CHECK(getApiTable().core.hsa_code_object_reader_destroy_fn(asHsaType()));
+llvm::Error CodeObjectReader::destroy() {
+  return LUTHIER_HSA_SUCCESS_CHECK(
+      getApiTable().core.hsa_code_object_reader_destroy_fn(asHsaType()));
 }
+
+} // namespace luthier::hsa
--- src/hsa_code_object_reader.hpp
+++ src/hsa_code_object_reader.hpp
@@ -1,8 +1,9 @@
 #ifndef HSA_CODE_OBJECT_READER_HPP
 #define HSA_CODE_OBJECT_READER_HPP
-#include <llvm/ADT/StringRef.h>
-#include <llvm/ADT/ArrayRef.h>
 #include "hsa_handle_type.hpp"
+#include <llvm/ADT/ArrayRef.h>
+#include <llvm/ADT/StringRef.h>
+#include <llvm/Support/Error.h>
 
 namespace luthier {
 
@@ -11,20 +12,22 @@ class CodeObjectManager;
 namespace hsa {
 
 class CodeObjectReader : public HandleType<hsa_code_object_reader_t> {
-    friend class luthier::CodeObjectManager;
+  friend class luthier::CodeObjectManager;
 
- private:
-    static CodeObjectReader createFromMemory(llvm::StringRef elf);
+private:
+  static llvm::Expected<CodeObjectReader> createFromMemory(llvm::StringRef elf);
 
-    static CodeObjectReader createFromMemory(llvm::ArrayRef<uint8_t> elf);
+  static llvm::Expected<CodeObjectReader>
+  createFromMemory(llvm::ArrayRef<uint8_t> elf);
 
-    void destroy();
+  llvm::Error destroy();
 
- public:
-    explicit CodeObjectReader(hsa_code_object_reader_t reader) : HandleType(reader){};
+public:
+  explicit CodeObjectReader(hsa_code_object_reader_t reader)
+      : HandleType(reader){};
 };
 
-}// namespace hsa
-}// namespace luthier
+} // namespace hsa
+} // namespace luthier
 
 #endif
