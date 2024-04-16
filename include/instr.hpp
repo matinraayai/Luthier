65db1bd50ab98fb6ea6df0b90267522fc6ff1b89
matinraayai <30674652+matinraayai@users.noreply.github.com>
2024-04-16 16:15:16 -0400

    Made instr.*pp public, moving towards a C++ API instead of a C API since there's no proper llvm C handles.

---
+++ include/instr.hpp
@@ -0,0 +1,56 @@
+#ifndef INSTR_HPP
+#define INSTR_HPP
+#include <llvm/MC/MCInst.h>
+
+#include "luthier_types.h"
+
+namespace luthier {
+
+class CodeLifter;
+
+/**
+ * \brief represents an AMDGPU instruction loaded by ROCr
+ * It is backed by an \p hsa::ExecutableSymbol of type
+ * \p HSA_SYMBOL_KIND_INDIRECT_FUNCTION and \p HSA_SYMBOL_KIND_KERNEL
+ */
+class Instr {
+private:
+  friend class luthier::CodeLifter; // CodeLifter is the only class allowed to
+                                    // construct a luthier::Instr
+                                    // CodeLifter is an internal component and
+                                    // cannot be accessed externally
+  const llvm::MCInst Inst; // < The MC representation of the instruction
+  const luthier_address_t LoadedDeviceAddress; // < The address on the device
+                                               // this instruction is loaded at
+  const hsa_executable_symbol_t
+      Symbol; // < The symbol this instruction belongs to
+  const hsa_loaded_code_object_t LCO; // < The Loaded Code Object this
+                                      // instruction belongs to This is cached
+                                      // to avoid looking it up using the symbol
+
+  const size_t Size; // < Size of the instruction
+
+  Instr(llvm::MCInst Inst, hsa_loaded_code_object_t LCO,
+        hsa_executable_symbol_t Symbol, luthier_address_t Address,
+        size_t Size);
+
+public:
+  Instr() = delete;
+
+  [[nodiscard]] hsa_agent_t getAgent() const;
+
+  [[nodiscard]] hsa_executable_t getExecutable() const;
+
+  [[nodiscard]] hsa_loaded_code_object_t getLoadedCodeObject() const;
+
+  [[nodiscard]] hsa_executable_symbol_t getExecutableSymbol() const;
+
+  [[nodiscard]] llvm::MCInst getInstr() const;
+
+  [[nodiscard]] luthier_address_t getLoadedDeviceAddress() const;
+
+  [[nodiscard]] size_t getSize() const;
+};
+} // namespace luthier
+
+#endif
---
+++ src/instr.cpp
@@ -0,0 +1,37 @@
+#include "instr.hpp"
+
+#include <utility>
+
+#include "hsa_agent.hpp"
+#include "hsa_executable.hpp"
+#include "hsa_executable_symbol.hpp"
+
+namespace luthier {
+
+Instr::Instr(llvm::MCInst Inst, hsa_loaded_code_object_t LCO,
+             hsa_executable_symbol_t Symbol, luthier_address_t Address,
+             size_t Size)
+    : Inst(std::move(Inst)), LCO(LCO), Symbol(Symbol),
+      LoadedDeviceAddress(Address), Size(Size) {}
+
+hsa_executable_t Instr::getExecutable() const {
+  return hsa::ExecutableSymbol::fromHandle(Symbol).getExecutable()->asHsaType();
+}
+
+hsa_loaded_code_object_t Instr::getLoadedCodeObject() const { return LCO; }
+
+hsa_agent_t Instr::getAgent() const {
+  return hsa::ExecutableSymbol::fromHandle(Symbol).getAgent()->asHsaType();
+}
+
+hsa_executable_symbol_t Instr::getExecutableSymbol() const { return Symbol; }
+
+llvm::MCInst Instr::getInstr() const { return Inst; }
+
+luthier_address_t Instr::getLoadedDeviceAddress() const {
+  return LoadedDeviceAddress;
+}
+
+size_t Instr::getSize() const { return Size; }
+
+} // namespace luthier
