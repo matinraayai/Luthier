3f12a949536e1eb3f6847bc4a9d7a585ed3d0ad0
matinraayai <30674652+matinraayai@users.noreply.github.com>
2024-04-23 11:47:55 -0400

    Updated intercept examples.

--- examples/intercept/intercept_hip.cpp
+++ examples/intercept/intercept_hip.cpp
@@ -1,32 +1,35 @@
-#include <luthier.h>
 #include <fstream>
 #include <functional>
 #include <hip/hip_runtime_api.h>
 #include <hsa/hsa.h>
 #include <hsa/hsa_ven_amd_loader.h>
 #include <iostream>
+#include <luthier/luthier.h>
 #include <string>
 
-void luthier_at_hsa_event(hsa_api_evt_args_t* cb_data, luthier_api_evt_phase_t phase, hsa_api_evt_id_t api_id) {}
+namespace luthier {
 
-void luthier_at_init() {
-    std::cout << "Calling luthier_enable_hip_op_callback" << std::endl;
-    //luthier_enable_hip_op_callback(108);
-    luthier_enable_all_hip_callbacks();
+void luthier_at_hsa_event(hsa_api_evt_args_t *cb_data,
+                          luthier_api_evt_phase_t phase,
+                          hsa_api_evt_id_t api_id) {}
+
+void luthier::atHsaApiTableLoad() {
+  std::cout << "Calling luthier_enable_hip_op_callback" << std::endl;
+  // luthier_enable_hip_op_callback(108);
+  luthier_enable_all_hip_callbacks();
 }
 
-
-void luthier_at_term() {
-    std::cout << "HIP Intercept Tool is terminating!" << std::endl;
+void luthier::atHsaApiTableUnload() {
+  std::cout << "HIP Intercept Tool is terminating!" << std::endl;
 }
 
-
-void luthier_at_hip_event(void* args, luthier_api_evt_phase_t phase, int hip_api_id) {
-    //if (hip_api_id == 108) luthier_enable_hip_all_callback();
-    fprintf(stdout, "<call to %d (%s)\t on %s> ",
-            hip_api_id,
-            hip_api_name(hip_api_id),
-            phase == LUTHIER_API_EVT_PHASE_ENTER ? "entry" : "exit"
-            );
-    fprintf(stdout, "\n"); fflush(stdout);
+void luthier_at_hip_event(void *args, luthier_api_evt_phase_t phase,
+                          int hip_api_id) {
+  // if (hip_api_id == 108) luthier_enable_hip_all_callback();
+  fprintf(stdout, "<call to %d (%s)\t on %s> ", hip_api_id,
+          hip_api_name(hip_api_id),
+          phase == LUTHIER_API_EVT_PHASE_ENTER ? "entry" : "exit");
+  fprintf(stdout, "\n");
+  fflush(stdout);
 }
+} // namespace luthier
--- examples/intercept/intercept_hsa.cpp
+++ examples/intercept/intercept_hsa.cpp
@@ -1,37 +1,32 @@
-#include <luthier.h>
-#include <fstream>
-#include <functional>
-#include <hip/hip_runtime_api.h>
-#include <hsa/hsa.h>
-#include <hsa/hsa_ven_amd_loader.h>
 #include <iostream>
+#include <luthier/luthier.h>
 #include <string>
 
-void luthier_at_init() {
-    std::cout << "Calling luthier_enable_hsa_op_callback" << std::endl;
-    //luthier_enable_hsa_op_callback(HSA_API_ID_hsa_signal_load_relaxed);
-    //luthier_enable_hsa_op_callback(HSA_API_ID_hsa_signal_create);
-    //luthier_enable_hsa_op_callback(HSA_API_ID_hsa_amd_memory_pool_free);
-    //luthier_disable_hsa_op_callback(HSA_EVT_ID_hsa_queue_packet_submit);
-    luthier_enable_all_hsa_callbacks();
+namespace luthier {
+
+void hsa::atHsaApiTableLoad() {
+  std::cout << "Calling enableHsaOpCallback" << std::endl;
+  // luthier_enable_hsa_op_callback(HSA_API_ID_hsa_signal_load_relaxed);
+  // luthier_enable_hsa_op_callback(HSA_API_ID_hsa_signal_create);
+  // luthier_enable_hsa_op_callback(HSA_API_ID_hsa_amd_memory_pool_free);
+  // luthier_disable_hsa_op_callback(HSA_EVT_ID_hsa_queue_packet_submit);
+  hsa::enableAllHsaCallbacks();
 }
 
-
-void luthier_at_term() {
-    std::cout << "HSA Intercept Tool is terminating!" << std::endl;
+void hsa::atHsaApiTableUnload() {
+  std::cout << "HSA Intercept Tool is terminating!" << std::endl;
 }
 
-void luthier_at_hip_event(void* args, luthier_api_evt_phase_t phase, int hip_api_id) {
-    //pass
+void hsa::atHsaEvt(luthier::hsa::ApiEvtArgs *CBData, luthier::ApiEvtPhase Phase,
+                   luthier::hsa::ApiEvtID ApiID) {
+  // if (api_id == HSA_API_ID_hsa_signal_load_relaxed)
+  // luthier_disable_hsa_op_callback(HSA_API_ID_hsa_signal_load_relaxed); if
+  // (api_id == HSA_API_ID_hsa_signal_create)
+  // luthier_disable_hsa_all_callback();
+  fprintf(stdout, "<call to (%d)\t on %s> ", ApiID,
+          Phase == luthier::API_EVT_PHASE_ENTER ? "entry" : "exit");
+  fprintf(stdout, "\n");
+  fflush(stdout);
 }
 
-
-void luthier_at_hsa_event(hsa_api_evt_args_t* cb_data, luthier_api_evt_phase_t phase, hsa_api_evt_id_t api_id) {
-    //if (api_id == HSA_API_ID_hsa_signal_load_relaxed) luthier_disable_hsa_op_callback(HSA_API_ID_hsa_signal_load_relaxed);
-    //if (api_id == HSA_API_ID_hsa_signal_create) luthier_disable_hsa_all_callback();
-    fprintf(stdout, "<call to (%d)\t on %s> ",
-            api_id,
-            phase == LUTHIER_API_EVT_PHASE_ENTER ? "entry" : "exit"
-            );
-    fprintf(stdout, "\n"); fflush(stdout);
-}
+} // namespace luthier
