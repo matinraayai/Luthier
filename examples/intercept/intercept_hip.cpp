3a23df88f3c803ccf39dfa6a4b1ac79d0c465d2e
mayakahlo <delossantos.m@northeastern.edu>
2024-04-28 21:16:25 -0400

    Implemented hipSuccess checks for hip functions called in intercept_hip.cpp

--- examples/intercept/intercept_hip.cpp
+++ examples/intercept/intercept_hip.cpp
@@ -1,20 +1,18 @@
-#include <luthier.h>
-#include <fstream>
-#include <functional>
-#include <hip/hip_runtime_api.h>
-#include <hsa/hsa.h>
-#include <hsa/hsa_ven_amd_loader.h>
-#include <iostream>
-#include <string>
-
-void luthier_at_hsa_event(hsa_api_evt_args_t* cb_data, luthier_api_evt_phase_t phase, hsa_api_evt_id_t api_id) {}
-
-void luthier_at_init() {
-    std::cout << "Calling luthier_enable_hip_op_callback" << std::endl;
-    //luthier_enable_hip_op_callback(108);
-    luthier_enable_all_hip_callbacks();
-}
-
+//#include <fstream>
+//#include <functional>
+//#include <hip/hip_runtime_api.h>
+//#include <hsa/hsa.h>
+//#include <hsa/hsa_ven_amd_loader.h>
+//
+//// Line 41 replaces this
+//// void luthier_at_hsa_event(hsa_api_evt_args_t* cb_data, luthier_api_evt_phase_t phase, hsa_api_evt_id_t api_id) {}
+//
+//void luthier_at_init() {
+//    std::cout << "Calling luthier_enable_hip_op_callback" << std::endl;
+//    //luthier_enable_hip_op_callback(108);
+//    luthier_enable_all_hip_callbacks();
+//}
+//
 
 #include <iostream>
 #include <luthier/luthier.h>
@@ -46,8 +44,21 @@ void hsa::atHsaEvt(luthier::hsa::ApiEvtArgs *CBData, luthier::ApiEvtPhase Phase,
     // want to call hipMalloc and check all the hip functions for their error codes and
     // make sure they equate to success
     // look at <hip/hip_runtime_api.h>
-    hipMallocManaged(&pointer, 20); // TODO: outputs status code make sure it is equal to hip_success
-    hipMemset(pointer, 5, 4);
+    hipError_t mallocError = hipMallocManaged(&pointer, 20); // TODO: outputs status code make sure it is equal to hipSuccess
+
+    if(mallocError != hipSuccess){
+      fprintf(stderr, "error: '%s'(%d) at %s:%d\n", hipGetErrorString(mallocError),  \
+              mallocError, __FILE__, __LINE__);                                      \
+      exit(EXIT_FAILURE);
+    }
+
+    hipError_t memsetError = hipMemset(pointer, 5, 4);
+
+    if(memsetError != hipSuccess){
+      fprintf(stderr, "error: '%s'(%d) at %s:%d\n", hipGetErrorString(memsetError),  \
+              memsetError, __FILE__, __LINE__);                                      \
+      exit(EXIT_FAILURE);
+    }
 
     // TODO: Also print out thread number
     std::cout << "Content of pointer: " << *pointer << std::endl;
