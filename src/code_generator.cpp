a9fca418c514d34d04ad20888ce79bcfbee13eb0
matinraayai <30674652+matinraayai@users.noreply.github.com>
2024-05-02 20:11:41 -0400

    Removed old commented-out code.

--- src/code_generator.cpp
+++ src/code_generator.cpp
@@ -136,8 +136,7 @@ llvm::Error CodeGenerator::compileRelocatableToExecutable(
 llvm::Error CodeGenerator::instrument(
     std::unique_ptr<llvm::Module> Module,
     std::unique_ptr<llvm::MachineModuleInfoWrapperPass> MMIWP,
-    const LiftedSymbolInfo &LSO, luthier::InstrumentationTask &ITask,
-    int *Addrs) {
+    const LiftedSymbolInfo &LSO, luthier::InstrumentationTask &ITask) {
   LUTHIER_LOG_FUNCTION_CALL_START
   auto Symbol = hsa::ExecutableSymbol::fromHandle(LSO.getSymbol());
   auto Agent = Symbol.getAgent();
@@ -154,164 +153,6 @@ llvm::Error CodeGenerator::instrument(
   llvm::SmallVector<char> Reloc;
   llvm::SmallVector<uint8_t> Executable;
 
-  //  llvm::Expected<hsa::ExecutableSymbol> InstrumentationFunc =
-  //      CodeObjectManager.getInstrumentationFunction(DeviceFunc, Agent);
-  //  LUTHIER_RETURN_ON_ERROR(InstrumentationFunc.takeError());
-  //
-  //  LUTHIER_RETURN_ON_ERROR(luthier::CodeLifter::instance().liftAndAddToModule(
-  //      *InstrumentationFunc, *Module, MMIWP->getMMI()
-  //      ));
-  //
-  //  auto MCInstInfo = TM->getMCInstrInfo();
-
-  // Get the Symbol
-  //  auto InstLLVMFunction =
-  //  Module->getFunction(llvm::cantFail(InstrumentationFunc->getName()));
-  ////  for (auto& MBB: *MMIWP->getMMI().getMachineFunction(*InstLLVMFunction))
-  ///{ /    for (auto& MI: MBB) { /      if (MI.getOpcode() ==
-  /// llvm::AMDGPU::S_GETPC_B64_vi) { /        MI.getNextNode() /      } /    }
-  ////  }
-  //  for (auto &F : *Module) {
-  //    if (F.getName() == KernelName->substr(0, KernelName->rfind(".kd"))) {
-  //      auto MF = MMIWP->getMMI().getMachineFunction(F);
-  //      MF->getFrameInfo().setHasCalls(true);
-  //      MF->getProperties().set(llvm::MachineFunctionProperties::Property::TracksLiveness);
-  //      auto MFI = MF->getInfo<llvm::SIMachineFunctionInfo>();
-  //      //    MFI->setScratchRSrcReg(llvm::AMDGPU::SGPR6_SGPR7);
-  //      auto TII = reinterpret_cast<const llvm::SIInstrInfo *>(
-  //          TM->getSubtargetImpl(F)->getInstrInfo());
-  //      //    MF->getProperties().reset(
-  //      //        llvm::MachineFunctionProperties::Property::NoVRegs);
-  //      auto &MRI = MF->getRegInfo();
-  //      auto MBBIter = MF->end();
-  //      MBBIter--;
-  //      auto &MBB = *MBBIter;
-  //      //      llvm::Register PCReg =
-  //      //          MRI.createVirtualRegister(&llvm::AMDGPU::SReg_64RegClass);
-  //      auto I = MBB.end();
-  //      I--;
-  //      llvm::MachineInstrBuilder Builder; // =
-  //      BuildMI(MBB, I, llvm::DebugLoc(),
-  //              MCInstInfo->get(llvm::AMDGPU::ADJCALLSTACKUP))
-  //          .addImm(0)
-  //          .addImm(0);
-  //      auto Address = reinterpret_cast<luthier_address_t>(
-  //          llvm::cantFail(InstrumentationFunc->getMachineCode()).data());
-  //      llvm::outs() << llvm::formatv("Address: {0:x}\n", Address);
-  //
-  //      auto MIB = llvm::BuildMI(MBB, I, llvm::DebugLoc(),
-  //      MCInstInfo->get(llvm::AMDGPU::SI_PC_ADD_REL_OFFSET))
-  //                                    .addDef(llvm::AMDGPU::SGPR4_SGPR5,
-  //                                    llvm::RegState::Define);
-  //      MIB.addGlobalAddress(InstLLVMFunction, 0,
-  //      llvm::SIInstrInfo::MO_REL32); MIB.addGlobalAddress(InstLLVMFunction,
-  //      0, llvm::SIInstrInfo::MO_REL32 + 1);
-  //
-  //      Builder = BuildMI(MBB, I, llvm::DebugLoc(),
-  //                        MCInstInfo->get(llvm::AMDGPU::SI_CALL_ISEL))
-  //                    .addReg(llvm::AMDGPU::SGPR4_SGPR5,
-  //                            llvm::RegState::Kill).addGlobalAddress(InstLLVMFunction);
-  ////      Builder; // =
-  //      BuildMI(MBB, I, llvm::DebugLoc(),
-  //              MCInstInfo->get(llvm::AMDGPU::ADJCALLSTACKDOWN))
-  //          .addImm(0)
-  //          .addImm(0);
-  //    }
-
-  //    //	s_add_u32 s4, s4, myCounter@rel32@lo+4
-  //    //	s_addc_u32 s5, s5, myCounter@rel32@hi+12
-  //    //	global_load_dwordx2 v[0:1], v0, s[4:5]
-  //    //	s_waitcnt vmcnt(0)
-  //    //	global_load_dword v2, v[0:1], off
-  //    //	s_waitcnt vmcnt(0)
-  //    //	v_add_u32_e32 v2, 1, v2
-  //    //	global_store_dword v[0:1], v2, off
-  //    //	s_waitcnt vmcnt(0)
-  //
-  //
-  //    //
-  //    //      // We need to compute the offset relative to the instruction
-  //    //      immediately
-  //    //      // after s_getpc_b64. Insert pc arithmetic code before last
-  //    //      terminator.
-  //    //    llvm::MachineInstr *GetPC =
-  //    //        BuildMI(MBB, I, llvm::DebugLoc(),
-  //    //                MCInstInfo->get(llvm::AMDGPU::S_GETPC_B64),
-  //    //                llvm::AMDGPU::SGPR4_SGPR5);
-  //    //
-  //    //
-  //    //
-  //    ////      auto SymLow =
-  //    ///
-  //    llvm::MachineOperand::CreateES(llvm::cantFail(instrumentationFunc->getName()).c_str(),
-  //    /// llvm::SIInstrInfo::MO_REL32_LO);
-  //    //
-  //    ////      auto SymHigh =
-  //    ///
-  //    llvm::MachineOperand::CreateES(llvm::cantFail(instrumentationFunc->getName()).c_str(),
-  //    /// llvm::SIInstrInfo::MO_REL32_HI);
-  //    //
-  //    //      llvm::outs() <<
-  //    //      llvm::cantFail(instrumentationFunc->getName()).c_str() << "\n";
-  //    //
-  //
-
-  ////    Builder->addOperand(llvm::MachineOperand::CreateReg(
-  ////        llvm::AMDGPU::EXEC, false, false, false, true));
-  //    std::string error;
-  //    llvm::StringRef errorRef(error);
-  //    //
-  ////    bool isInstCorrect = TII->verifyInstruction(*Builder, errorRef);
-  //    //
-  ////    llvm::outs() << "Is inst correct? " << isInstCorrect << "\n";
-  ////    llvm::outs() << errorRef << "\n";
-  //
-
-  ////          .addReg(llvm::AMDGPU::SGPR4, llvm::RegState::Define);
-  //
-  //    Builder = BuildMI(MBB, I, llvm::DebugLoc(),
-  //                      MCInstInfo->get(llvm::AMDGPU::S_MOV_B32_vi))
-  //                  .addReg(llvm::AMDGPU::SGPR5, llvm::RegState::Define)
-  //                  .addImm(static_cast<int64_t>(Address >> 32));
-  //
-  //    auto VirtReg =
-  //    MRI.createVirtualRegister(&llvm::AMDGPU::SReg_128RegClass); Builder =
-  //    BuildMI(MBB, I, llvm::DebugLoc(),
-  //                      MCInstInfo->get(llvm::AMDGPU::WWM_COPY))
-  //                  .addReg(llvm::AMDGPU::SGPR0_SGPR1_SGPR2_SGPR3,
-  //                  llvm::RegState::Define)
-  //                  .addReg(llvm::AMDGPU::PRIVATE_RSRC_REG);
-
-  //    //    .addReg(llvm::AMDGPU::SGPR5, llvm::RegState::Define)
-  //    //        .addImm(static_cast<int64_t>(Address >> 32));
-  //
-
-  //
-  //            //          .addReg(llvm::AMDGPU::SGPR5,
-  //            llvm::RegState::Define);
-  //            ////      Builder->addOperand(SymHigh);
-  //            ////
-  //            bool isInstCorrect = TII->verifyInstruction(*Builder, errorRef);
-  //    ////
-  //    //      llvm::outs() << "Is inst correct? " << isInstCorrect << "\n";
-  //    //      llvm::outs() << errorRef << "\n";
-  //    Builder = llvm::BuildMI(MBB, I, llvm::DebugLoc(),
-  //                            MCInstInfo->get(llvm::AMDGPU::S_SWAPPC_B64_vi))
-  //                  .addReg(llvm::AMDGPU::SGPR30_SGPR31,
-  //                  llvm::RegState::Define)
-  //                  .addReg(llvm::AMDGPU::SGPR4_SGPR5);
-  //
-  //    isInstCorrect = TII->verifyInstruction(*Builder, errorRef);
-  //    //
-  //    llvm::outs() << "Is inst correct? " << isInstCorrect << "\n";
-  //    llvm::outs() << errorRef << "\n";
-  //      I--;
-  //      auto LastMBB = MBB.splitAt(*I);
-  //      MF->getFrameInfo().setHasCalls(true);
-  //    MF->print(llvm::outs());
-  //  }
-  //  Module->getFunctionList().begin()->ge
-  //  MMIWP->getMMI().get
   llvm::MCContext &MCContext = MMIWP->getMMI().getContext();
 
   auto AddedLSIs = applyInstrumentation(*Module, MMIWP->getMMI(), LSO, ITask);
@@ -327,51 +168,11 @@ llvm::Error CodeGenerator::instrument(
   llvm::TargetPassConfig *TPC = TM.createPassConfig(PM);
 
   PM.add(TPC);
-  //  TPC->addISelPasses();
-  //  TPC->addPrintPass("My stuff:");
   PM.add(MMIWP.release());
   TPC->addISelPasses();
-  PM.add(llvm::createMachineFunctionPrinterPass(llvm::outs(),
-                                                "After ISEL Passes"));
-  TPC->printAndVerify("After ISEL Passes");
-  //  PM.add(llvm::Pass::createPass(&llvm::FinalizeISelID));
-  //  PM.add(llvm::Pass::createPass(&llvm::SIFixSGPRCopiesID));
-  //  PM.add(llvm::createSILowerI1CopiesPass());
   TPC->addMachinePasses();
-  PM.add(llvm::createMachineFunctionPrinterPass(llvm::outs(),
-                                                "After Machine Passes"));
-  //  TPC->printAndVerify("After Machine Passes");
-
-  //  TPC->addPass(&llvm::FinalizeISelID);
-
-  //        TPC->printAndVerify("MachineFunctionGenerator::assemble");
-
-  //        auto usageAnalysis =
-  //        std::make_unique<llvm::AMDGPUResourceUsageAnalysis>();
   auto UsageAnalysis = new llvm::AMDGPUResourceUsageAnalysis();
   PM.add(UsageAnalysis);
-  PM.add(llvm::createMachineFunctionPrinterPass(llvm::outs(),
-                                                "After Usage Analysis"));
-  //  TPC->printAndVerify("After Resource Usage");
-  // Add target-specific passes.
-  //        ET.addTargetSpecificPasses(PM);
-  //        TPC->printAndVerify("After
-  //        ExegesisTarget::addTargetSpecificPasses");
-  // Adding the following passes:
-  // - postrapseudos: expands pseudo return instructions used on some targets.
-  // - machineverifier: checks that the MachineFunction is well formed.
-  // - prologepilog: saves and restore callee saved registers.
-  //        for (const char *PassName :
-  //             {"postrapseudos", "machineverifier", "prologepilog"})
-  //            if (addPass(PM, PassName, *TPC))
-  //                return make_error<Failure>("Unable to add a mandatory
-  //                pass");
-  //  TPC->setInitialized();
-
-  //        llvm::SmallVector<char> o;
-  //        std::string out;
-  //        llvm::raw_string_ostream outOs(out);
-
   TPC->setInitialized();
   llvm::raw_svector_ostream OutOS(Reloc);
 
@@ -379,14 +180,10 @@ llvm::Error CodeGenerator::instrument(
   if (TM.addAsmPrinter(PM, OutOS, nullptr, llvm::CodeGenFileType::ObjectFile,
                        MCContext))
     llvm::outs() << "Failed to add pass manager\n";
-  //            return make_error<llvm::Failure>("Cannot add AsmPrinter
-  //            passes");
 
   PM.run(*Module); // Run all the passes
   std::error_code code;
   llvm::raw_fd_ostream("my_reloc.hsaco", code) << Reloc;
-  //  llvm::outs()
-  llvm::outs() << Reloc << "\n";
 
   LUTHIER_RETURN_ON_ERROR(compileRelocatableToExecutable(
       llvm::ArrayRef<uint8_t>(reinterpret_cast<uint8_t *>(Reloc.data()),
@@ -396,32 +193,17 @@ llvm::Error CodeGenerator::instrument(
 
   llvm::outs() << llvm::toStringRef(Executable) << "\n";
 
-  //  llvm::outs() << elfFile->get()->getRelSection();
-
   std::vector<hsa::ExecutableSymbol> ExternGVs;
-  for (const auto& GV: LSO.getRelatedVariables())
+  for (const auto &GV : LSO.getRelatedVariables())
     ExternGVs.push_back(hsa::ExecutableSymbol::fromHandle(GV));
 
-  for (const auto& L : *AddedLSIs)
-    for (const auto& GV : L.getRelatedVariables())
+  for (const auto &L : *AddedLSIs)
+    for (const auto &GV : L.getRelatedVariables())
       ExternGVs.push_back(hsa::ExecutableSymbol::fromHandle(GV));
 
-  LUTHIER_RETURN_ON_ERROR(CodeObjectManager.loadInstrumentedKernel(
-      Executable, Symbol, ExternGVs, Addrs));
+  LUTHIER_RETURN_ON_ERROR(
+      CodeObjectManager.loadInstrumentedKernel(Executable, Symbol, ExternGVs));
 
-  //  auto instFunctionInstructions =
-  //      CodeLifter::instance().disassemble(*InstrumentationFunc);
-  //  LUTHIER_RETURN_ON_ERROR(instFunctionInstructions.takeError());
-  //  //
-  //  for (const auto &i : **instFunctionInstructions) {
-  //    std::string instStr;
-  //    llvm::raw_string_ostream instStream(instStr);
-  //    auto inst = i.getInstr();
-  //    TargetInfo->getMCInstPrinter()->printInst(
-  //        &inst, reinterpret_cast<luthier_address_t>(i.getAddress()), "",
-  //        *TargetInfo->getMCSubTargetInfo(), llvm::outs());
-  //    llvm::outs() << "\n";
-  //  }
   return llvm::Error::success();
 }
 
@@ -467,8 +249,8 @@ CodeGenerator::insertFunctionCalls(
                   .addImm(0);
     auto &CalleeMF = IFLSI->getMFofSymbol();
     auto &CalleeFunction = CalleeMF.getFunction();
-    MBB->getParent()->dump();
-    llvm::outs() << " ======== \n";
+    //    MBB->getParent()->dump();
+    //    llvm::outs() << " ======== \n";
     Builder =
         llvm::BuildMI(*MBB, Builder.getInstr()->getNextNode(), llvm::DebugLoc(),
                       MCInstInfo->get(llvm::AMDGPU::SI_PC_ADD_REL_OFFSET))
@@ -477,8 +259,8 @@ CodeGenerator::insertFunctionCalls(
                               llvm::SIInstrInfo::MO_REL32_LO)
             .addGlobalAddress(&CalleeFunction, 0,
                               llvm::SIInstrInfo::MO_REL32_HI);
-    MBB->getParent()->dump();
-    llvm::outs() << " ======== \n";
+    //    MBB->getParent()->dump();
+    //    llvm::outs() << " ======== \n";
     auto SaveReg = MBB->getParent()->getRegInfo().createVirtualRegister(
         &llvm::AMDGPU::SGPR_128RegClass);
     Builder =
@@ -486,15 +268,15 @@ CodeGenerator::insertFunctionCalls(
                       MCInstInfo->get(llvm::AMDGPU::COPY))
             .addDef(SaveReg)
             .addReg(llvm::AMDGPU::PRIVATE_RSRC_REG);
-    MBB->getParent()->dump();
-    llvm::outs() << " ======== \n";
+    //    MBB->getParent()->dump();
+    //    llvm::outs() << " ======== \n";
     Builder =
         llvm::BuildMI(*MBB, Builder.getInstr()->getNextNode(), llvm::DebugLoc(),
                       MCInstInfo->get(llvm::AMDGPU::COPY))
             .addDef(llvm::AMDGPU::SGPR0_SGPR1_SGPR2_SGPR3)
             .addReg(SaveReg);
-    MBB->getParent()->dump();
-    llvm::outs() << " ======== \n";
+    //    MBB->getParent()->dump();
+    //    llvm::outs() << " ======== \n";
     auto TRI = CalleeMF.getSubtarget<llvm::GCNSubtarget>().getRegisterInfo();
     auto RegMask =
         TRI->getCallPreservedMask(CalleeMF, CalleeFunction.getCallingConv());
@@ -509,15 +291,15 @@ CodeGenerator::insertFunctionCalls(
                 llvm::AMDGPU::SGPR0_SGPR1_SGPR2_SGPR3, false, true))
             .addRegMask(RegMask);
 
-    MBB->getParent()->dump();
-    llvm::outs() << " ======== \n";
+    //    MBB->getParent()->dump();
+    //    llvm::outs() << " ======== \n";
     Builder =
         llvm::BuildMI(*MBB, Builder.getInstr()->getNextNode(), llvm::DebugLoc(),
                       MCInstInfo->get(llvm::AMDGPU::ADJCALLSTACKDOWN))
             .addImm(0)
             .addImm(0);
-    MBB->getParent()->dump();
-    llvm::outs() << " ======== \n";
+    //    MBB->getParent()->dump();
+    //    llvm::outs() << " ======== \n";
     //                                    .addDef(llvm::AMDGPU::SGPR4_SGPR5,
     //                                    llvm::RegState::Define);
     //      MIB.addGlobalAddress(InstLLVMFunction, 0,
