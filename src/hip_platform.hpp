615b0b97fac7985a159c4d194d6280b2656ca4ae
matinraayai <30674652+matinraayai@users.noreply.github.com>
2024-05-02 21:02:13 -0400

    Added hip_platform.hpp.

---
+++ src/hip_platform.hpp
@@ -0,0 +1,31 @@
+#ifndef HIP_PLATFORM_HPP
+#define HIP_PLATFORM_HPP
+#include <luthier/hip_trace_api.h>
+#include <llvm/ADT/DenseMap.h>
+
+namespace luthier::hip {
+
+/**
+ * \brief in charge of caching useful information about the HIP runtime state
+ */
+class Platform {
+
+public:
+  Platform(const Platform &) = delete;
+  Platform &operator=(const Platform &) = delete;
+
+  static inline Platform &instance() {
+    static Platform Instance;
+    return Instance;
+  }
+
+private:
+  Platform() = default;
+  ~Platform() = default;
+
+
+};
+
+} // namespace luthier::hip
+
+#endif
