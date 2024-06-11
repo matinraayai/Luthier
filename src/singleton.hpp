d7015232344a269dbdd5a9c6967ab11e622a8eeb
matinraayai <30674652+matinraayai@users.noreply.github.com>
2024-06-11 01:07:41 -0400

    Added call_once to Singleton's constructor and destructor.

--- src/singleton.hpp
+++ src/singleton.hpp
@@ -11,26 +11,32 @@
 #ifndef SINGLETON_HPP
 #define SINGLETON_HPP
 #include <llvm/Support/ErrorHandling.h>
+#include <mutex>
 
 namespace luthier {
 
-///
-/// \tparam T
+/// \brief Interface inherited by all Singleton objects in Luthier
+/// \tparam T The concrete Singleton object itself
 template <typename T> class Singleton {
 private:
   static T *Instance;
 
 public:
   Singleton() {
-    if (Instance != nullptr) {
-      llvm::report_fatal_error("Called the singleton constructor twice.");
-    }
-    Instance = static_cast<T *>(this);
+    static std::once_flag OnceFlag;
+    std::call_once(OnceFlag, [&]() { Instance = static_cast<T *>(this); });
   }
-  ~Singleton() { Instance = nullptr; }
 
+  ///
+  ~Singleton() {
+    static std::once_flag OnceFlag;
+    std::call_once(OnceFlag, [&]() { Instance = nullptr; });
+  }
+
+  /// Disallowed copy construction
   Singleton(const Singleton &) = delete;
 
+  /// Disallowed assignment operation
   Singleton &operator=(const Singleton &) = delete;
 
   static inline T &instance() {
