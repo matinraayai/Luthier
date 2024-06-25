416817c87be17e067eb30ebb69c1fd3aa80ee2df
matinraayai <30674652+matinraayai@users.noreply.github.com>
2024-06-25 18:45:10 -0400

    Added docs.

--- src/cloning.hpp
+++ src/cloning.hpp
@@ -3,9 +3,9 @@
 //===----------------------------------------------------------------------===//
 ///
 /// \file
-/// This file contains utility functions used to clone LLVM IR and MIR
-/// Modules and Functions, used frequently by Luthier components involved in the
-/// code generation process.
+/// This file contains utility functions used to clone LLVM MIR constructs,
+/// used frequently by Luthier components involved in the code generation
+/// process. It is essentially a modified version of llvm-reduce.
 //===----------------------------------------------------------------------===//
 
 #ifndef CLONING_HPP
@@ -16,14 +16,20 @@
 #include <llvm/Transforms/Utils/ValueMapper.h>
 namespace luthier {
 
+/// Clones the content of the source machine function \p SrcMF into the
+/// The module of \p DestMMI should be a cloned version of \p SrcMF 's module,
+/// and the value map of the cloning operation must be passed in \p VMap
+/// \param SrcMF
+/// \param VMap
+/// \param DestMMI
+/// \return
 llvm::Expected<std::unique_ptr<llvm::MachineFunction>>
 cloneMF(const llvm::MachineFunction *SrcMF, const llvm::ValueToValueMapTy &VMap,
         llvm::MachineModuleInfo &DestMMI);
 
 /// Clones the content of the source Machine Module Info \p SrcMMI into
 /// the \p DestMMI. Uses the \p VMap to map the global objects in the source
-/// module into the destination, already cloned module\n
-/// Underlying logic has been copy-pasted from llvm-reduce\n
+/// module into the destination, already cloned module
 /// \param [in] SrcMMI the source \c llvm::MachineModuleInfo
 /// \param [in] VMap a mapping between a global object in the source module and
 /// its copy in the destination module
