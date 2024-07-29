87b6819066720e3565d6f4ee0031a6ce8dd5be67
matinraayai <30674652+matinraayai@users.noreply.github.com>
2024-07-28 21:04:27 -0400

    Added luthier tablegen binary project.

---
+++ luthier-tblgen/Main.cpp
@@ -0,0 +1,38 @@
+//===-- Main.cpp - main function of luthier-tblgen ------------------------===//
+//
+//===----------------------------------------------------------------------===//
+///
+/// \file
+/// This file contains utility functions used to clone LLVM MIR constructs,
+/// used frequently by Luthier components involved in the code generation
+/// process. It is essentially a modified version of llvm-reduce.
+//===----------------------------------------------------------------------===//
+#include <llvm/Support/CommandLine.h>
+#include <llvm/TableGen/Main.h>
+#include <llvm/TableGen/Record.h>
+
+#include "PseudoRealInstMapBackend.hpp"
+
+namespace {
+
+llvm::cl::opt<bool> GenerateSiRealToPseudoOpcode(
+    "si-real-to-pseudo-opcode-map",
+    llvm::cl::desc(
+        "Generate a Real to Pseudo Opcode map for the AMDGPU backend"));
+
+} // anonymous namespace
+
+namespace luthier {
+
+bool TableGenMain(llvm::raw_ostream &OS, llvm::RecordKeeper &Records) {
+  if (GenerateSiRealToPseudoOpcode)
+    EmitMapTable(Records, OS);
+  return false;
+}
+
+} // namespace luthier
+
+int main(int argc, char *argv[]) {
+  llvm::cl::ParseCommandLineOptions(argc, argv);
+  return TableGenMain(argv[0], &luthier::TableGenMain);
+}
