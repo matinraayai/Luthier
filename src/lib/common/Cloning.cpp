31397d54a95f91c00764f749ef7b422990bc2b55
matinraayai <30674652+matinraayai@users.noreply.github.com>
2024-08-19 11:17:24 -0400

    Renamed file and included headers.

--- src/lib/common/Cloning.cpp
+++ src/lib/common/Cloning.cpp
@@ -1,5 +1,24 @@
-#include "common/cloning.hpp"
-#include "common/error.hpp"
+//===-- Cloning.cpp - IR and MIR Cloning Utilities ------------------------===//
+// Copyright 2022-2024 @ Northeastern University Computer Architecture Lab
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//===----------------------------------------------------------------------===//
+///
+/// \file
+/// This file implements utility functions used to clone LLVM MIR constructs.
+//===----------------------------------------------------------------------===//
+#include "common/Cloning.hpp"
+#include "common/Error.hpp"
 #include <llvm/CodeGen/MachineBasicBlock.h>
 #include <llvm/CodeGen/MachineFrameInfo.h>
 #include <llvm/CodeGen/MachineFunction.h>
@@ -168,11 +187,10 @@ static void cloneMemOperands(llvm::MachineInstr &DstMI,
   DstMI.setMemRefs(DstMF, NewMMOs);
 }
 
-llvm::Expected<std::unique_ptr<llvm::MachineFunction>>
-cloneMF(const llvm::MachineFunction *SrcMF, const llvm::ValueToValueMapTy &VMap,
-        llvm::MachineModuleInfo &DestMMI,
-        llvm::DenseMap<llvm::MachineInstr *, llvm::MachineInstr *>
-            *SrcToDstMIMap) {
+llvm::Expected<std::unique_ptr<llvm::MachineFunction>> cloneMF(
+    const llvm::MachineFunction *SrcMF, const llvm::ValueToValueMapTy &VMap,
+    llvm::MachineModuleInfo &DestMMI,
+    llvm::DenseMap<llvm::MachineInstr *, llvm::MachineInstr *> *SrcToDstMIMap) {
   // Find the destination function entry in the value map
   auto &SrcF = SrcMF->getFunction();
   auto DestFMapEntry = VMap.find(&SrcF);
@@ -193,8 +211,7 @@ cloneMF(const llvm::MachineFunction *SrcMF, const llvm::ValueToValueMapTy &VMap,
   for (const llvm::MachineBasicBlock &SrcMBB : *SrcMF) {
     // We don't assign a BB to the MBB because it is likely to be just
     // a placeholder with an unreachable instruction
-    llvm::MachineBasicBlock *DstMBB =
-        DstMF->CreateMachineBasicBlock();
+    llvm::MachineBasicBlock *DstMBB = DstMF->CreateMachineBasicBlock();
     Src2DstMBB[const_cast<llvm::MachineBasicBlock *>(&SrcMBB)] = DstMBB;
 
     DstMBB->setCallFrameSize(SrcMBB.getCallFrameSize());
