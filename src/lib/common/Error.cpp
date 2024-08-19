1cd46b66a0c0077b2cc85e99f8e5d18cc934a394
matinraayai <30674652+matinraayai@users.noreply.github.com>
2024-08-19 11:18:14 -0400

    Added banner.

--- src/lib/common/Error.cpp
+++ src/lib/common/Error.cpp
@@ -1,4 +1,23 @@
-#include <common/error.hpp>
+//===-- Error.cpp - Luthier LLVM Error Types ------------------------------===//
+// Copyright 2022-2024 @ Northeastern University Computer Architecture Lab
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//===----------------------------------------------------------------------===//
+///
+/// \file
+/// This file implements different types of \c llvm::Error used by Luthier.
+//===----------------------------------------------------------------------===//
+#include <common/Error.hpp>
 
 char luthier::InvalidArgument::ID = 0;
 
