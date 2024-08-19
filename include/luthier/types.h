3c79cd50092a5e48f8bb477ae2bb357908083838
matinraayai <30674652+matinraayai@users.noreply.github.com>
2024-08-19 01:00:54 -0400

    Moved AqlPacket to its own header file + minor docs update and cleanup.

--- include/luthier/types.h
+++ include/luthier/types.h
@@ -1,8 +1,20 @@
 //===-- types.h - Luthier Types  --------------------------------*- C++ -*-===//
+// Copyright 2022-2024 @ Northeastern University Computer Architecture Lab
 //
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 //===----------------------------------------------------------------------===//
 /// \file
-/// This file describes simple types and structs used by Luthier.
+/// This file describes simple types and enums used by Luthier.
 //===----------------------------------------------------------------------===//
 #ifndef LUTHIER_TYPES_H
 #define LUTHIER_TYPES_H
@@ -19,18 +31,19 @@ namespace luthier {
 #define STRINGIFY(S) #S
 
 /// A macro to delay expansion of \c STRINGIFY
-#define X_STRINGIFY(S) STRINGIFY(S)
+#define STRINGIFY_DELAY(S) STRINGIFY(S)
 
-constexpr const char *HookHandlePrefix = X_STRINGIFY(HOOK_HANDLE_PREFIX);
+constexpr const char *HookHandlePrefix = STRINGIFY_DELAY(HOOK_HANDLE_PREFIX);
 
-constexpr const char *ReservedManagedVar = X_STRINGIFY(RESERVED_MANAGED_VAR);
+constexpr const char *ReservedManagedVar =
+    STRINGIFY_DELAY(RESERVED_MANAGED_VAR);
 
 #define LUTHIER_HOOK_ATTRIBUTE "luthier_hook"
 
 /// All bindings to Luthier intrinsics must have this attribute
 #define LUTHIER_INTRINSIC_ATTRIBUTE "luthier_intrinsic"
 
-#undef X_STRINGIFY
+#undef STRINGIFY_DELAY
 
 #undef STRINGIFY
 
@@ -45,119 +58,6 @@ enum ApiEvtPhase : unsigned short {
   API_EVT_PHASE_AFTER = 1
 };
 
-/// Points to before/after an \c llvm::MachineInstr
-enum InstrPoint : unsigned short {
-  INSTR_POINT_BEFORE = 0,
-  INSTR_POINT_AFTER = 1
-};
-
-namespace hsa {
-
-/// \brief AMD Vendor Packet POD struct
-typedef struct {
-  uint16_t Header;
-  uint8_t Format;
-  uint8_t Rest[61];
-} amd_vendor_packet_t;
-
-/// \brief POD struct to provide an abstraction over HSA AQL packets as well
-/// as some convenience methods
-/// \details This should not be constructed directly. It should only be obtained
-/// by using <tt>reinterpret_cast</tt> over the address of a packet:
-/// \code
-/// auto& Packet = *reinterpret_cast<HsaAqlPacket*>(PacketAddress);
-/// \endcode
-struct AqlPacket {
-  /// \brief Generic packet struct
-  struct {
-    uint16_t Header;
-    struct {
-      uint8_t UserData[62];
-    } Body;
-  } Packet;
-
-  /// \return the type of the packet
-  /// \sa hsa_packet_type_t
-  [[nodiscard]] hsa_packet_type_t getPacketType() const {
-    return static_cast<hsa_packet_type_t>(
-        (Packet.Header >> HSA_PACKET_HEADER_TYPE) &
-        ((1 << HSA_PACKET_HEADER_WIDTH_TYPE) - 1));
-  }
-
-  /// \return a non-const reference to this as a AMD vendor packet
-  /// \sa hsa_amd_vendor_packet_t
-  [[nodiscard]] amd_vendor_packet_t &asAMDVendor() {
-    return *reinterpret_cast<amd_vendor_packet_t *>(&Packet);
-  }
-
-  /// \return a non-const reference to this a AMD vendor packet
-  /// \sa hsa_amd_vendor_packet_t
-  [[nodiscard]] const amd_vendor_packet_t &asAMDVendor() const {
-    return *reinterpret_cast<const amd_vendor_packet_t *>(&Packet);
-  }
-
-  /// \return a non-const reference to this as a kernel dispatch packet
-  /// \sa hsa_kernel_dispatch_packet_t
-  [[nodiscard]] hsa_kernel_dispatch_packet_t &asKernelDispatch() {
-    return *reinterpret_cast<hsa_kernel_dispatch_packet_t *>(&Packet);
-  }
-
-  /// \return a const reference to this as a kernel dispatch packet
-  /// \sa hsa_kernel_dispatch_packet_t
-  [[nodiscard]] const hsa_kernel_dispatch_packet_t &asKernelDispatch() const {
-    return *reinterpret_cast<const hsa_kernel_dispatch_packet_t *>(&Packet);
-  }
-
-  /// \return a non-const reference to this as a Barrier-And packet
-  /// \sa hsa_barrier_and_packet_t
-  [[nodiscard]] hsa_barrier_and_packet_t &asBarrierAnd() {
-    return *reinterpret_cast<hsa_barrier_and_packet_t *>(&Packet);
-  }
-
-  /// \return a const reference to this as a Barrier-And packet
-  /// \sa hsa_barrier_and_packet_t
-  [[nodiscard]] const hsa_barrier_and_packet_t &asBarrierAnd() const {
-    return *reinterpret_cast<const hsa_barrier_and_packet_t *>(&Packet);
-  }
-
-  /// \return a non-const reference to this as a Barrier-Or packet
-  /// \sa hsa_barrier_or_packet_t
-  [[nodiscard]] hsa_barrier_or_packet_t &asBarrierOr() {
-    return *reinterpret_cast<hsa_barrier_or_packet_t *>(&Packet);
-  }
-
-  /// \return a const reference to this as a Barrier-Or packet
-  /// \sa hsa_barrier_or_packet_t
-  [[nodiscard]] const hsa_barrier_or_packet_t &asBarrierOr() const {
-    return *reinterpret_cast<const hsa_barrier_or_packet_t *>(&Packet);
-  }
-
-  /// \return a non-const reference to this as a Agent packet
-  /// \sa hsa_agent_dispatch_packet_t
-  [[nodiscard]] hsa_agent_dispatch_packet_t &asAgentDispatch() {
-    return *reinterpret_cast<hsa_agent_dispatch_packet_t *>(&Packet);
-  }
-
-  /// \return a const reference to this as a Agent packet
-  /// \sa hsa_agent_dispatch_packet_t
-  [[nodiscard]] const hsa_agent_dispatch_packet_t &asAgentDispatch() const {
-    return *reinterpret_cast<const hsa_agent_dispatch_packet_t *>(&Packet);
-  }
-};
-
-/// \brief HSA Symbol kinds; Use this instead of \c hsa_symbol_kind_t
-/// \details At the time of writing HSA only supports kernel and variable symbols;
-/// Support for GNU IFunc (i.e. <tt>INDIRECT_FUNCTION</tt> is not yet implemented,
-/// and device functions are not recognized by HSA but emitted in LLVM. Since
-/// Luthier needs to recognize device functions as an
-/// <tt>hsa_executable_symbol_t</tt>, it implements an abstraction layer which
-/// generates handles for each device function and exposes them to a tool writer.
-/// In order to do HSA operations on an executable symbol, one must make
-/// sure it is of type <tt>KERNEL</tt> or <tt>VARIABLE</tt>. Luthier implements
-/// and exposes device function symbol functionality separately
-enum SymbolKind : uint8_t { VARIABLE = 0, KERNEL = 1, DEVICE_FUNCTION = 2 };
-
-} // namespace hsa
 } // namespace luthier
 
 #endif
