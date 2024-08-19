af7fc5009fcb5499e5fe5fa68202fb6fcf506fdc
matinraayai <30674652+matinraayai@users.noreply.github.com>
2024-08-19 01:46:08 -0400

    Minor header renaming + added copyright banner.

--- src/include/hsa/HsaRuntimeInterceptor.hpp
+++ src/include/hsa/HsaRuntimeInterceptor.hpp
@@ -1,5 +1,17 @@
-//===-- hsa_intercept.hpp - HSA Runtime API Interceptor -------------------===//
+//===-- HsaRuntimeInterceptor.hpp - HSA Runtime API Interceptor -----------===//
+// Copyright 2022-2024 @ Northeastern University Computer Architecture Lab
 //
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 //===----------------------------------------------------------------------===//
 ///
 /// \file
@@ -15,10 +27,10 @@
 
 #include <llvm/ADT/DenseSet.h>
 
+#include "common/Error.hpp"
 #include "common/ROCmLibraryApiInterceptor.hpp"
-#include "common/error.hpp"
-#include "common/singleton.hpp"
-#include <luthier/hsa_trace_api.h>
+#include "common/Singleton.hpp"
+#include <luthier/hsa/TraceApi.h>
 #include <luthier/types.h>
 
 namespace luthier::hsa {
