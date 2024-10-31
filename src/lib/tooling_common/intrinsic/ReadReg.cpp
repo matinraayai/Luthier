206e01fe692e6b22b55a5fd3a3bd521d262c5200
matinraayai <30674652+matinraayai@users.noreply.github.com>
2024-10-31 14:36:35 -0400

    ReadReg.cpp/WriteExec.cpp: Added verbose error messages.

--- src/lib/tooling_common/intrinsic/ReadReg.cpp
+++ src/lib/tooling_common/intrinsic/ReadReg.cpp
@@ -13,17 +13,29 @@ namespace luthier {
 llvm::Expected<IntrinsicIRLoweringInfo>
 readRegIRProcessor(const llvm::Function &Intrinsic, const llvm::CallInst &User,
                    const llvm::GCNTargetMachine &TM) {
+  // The User must only have 1 operand
+  LUTHIER_RETURN_ON_ERROR(
+      LUTHIER_ERROR_CHECK(User.arg_size() == 1,
+                          "Expected one operand to be passed to the "
+                          "luthier::readReg intrinsic '{0}', got {1}.",
+                          User, User.arg_size()));
+
   auto *TRI = TM.getSubtargetImpl(Intrinsic)->getRegisterInfo();
   // The first argument specifies the MCRegister Enum that will be read
   // The enum value should be constant; A different intrinsic should be used
   // if reg indexing is needed at runtime
   auto *Arg = llvm::dyn_cast<llvm::ConstantInt>(User.getArgOperand(0));
-  LUTHIER_RETURN_ON_ERROR(LUTHIER_ASSERTION(Arg != nullptr));
+  LUTHIER_RETURN_ON_ERROR(LUTHIER_ERROR_CHECK(
+      Arg != nullptr, "The first argument of the luthier::readReg intrinsic is "
+                      "not a constant integer."));
   // Get the MCRegister from the first argument's content
   llvm::MCRegister Reg(Arg->getZExtValue());
   // Check if the enum value is indeed a physical register
   LUTHIER_RETURN_ON_ERROR(
-      LUTHIER_ASSERTION(llvm::MCRegister::isPhysicalRegister(Reg.id())));
+      LUTHIER_ERROR_CHECK(llvm::MCRegister::isPhysicalRegister(Reg.id()),
+                          "The first argument of the luthier::readReg {0}"
+                          "intrinsic is not an LLVM MCRegister.",
+                          Reg.id()));
   // Get the type of the read register and encode its inline asm constraint
   auto *PhysRegClass = TRI->getPhysRegBaseClass(Reg);
   std::string Constraint;
@@ -34,7 +46,10 @@ readRegIRProcessor(const llvm::Function &Intrinsic, const llvm::CallInst &User,
   else if (llvm::SIRegisterInfo::isSGPRClass(PhysRegClass))
     Constraint = "s";
   else
-    llvm_unreachable("register type not implemented");
+    return LUTHIER_CREATE_ERROR(
+        "Unable to find a suitable register class for reading "
+        "the MC Register {0}.",
+        Reg.id());
 
   luthier::IntrinsicIRLoweringInfo Out;
   // Set the output's constraint
@@ -56,8 +71,15 @@ llvm::Error readRegMIRProcessor(
     const std::function<llvm::Register(llvm::MCRegister)> &PhysRegAccessor,
     llvm::DenseMap<llvm::MCRegister, llvm::Register> &PhysRegsToBeOverwritten) {
   // There should be only a single virtual register involved in the operation
-  LUTHIER_RETURN_ON_ERROR(LUTHIER_ASSERTION(Args.size() == 1));
-  LUTHIER_RETURN_ON_ERROR(LUTHIER_ASSERTION(Args[0].first.isRegDefKind()));
+  LUTHIER_RETURN_ON_ERROR(
+      LUTHIER_ERROR_CHECK(Args.size() == 1,
+                          "Number of virtual register arguments "
+                          "involved in the MIR lowering stage of "
+                          "luthier::readReg is {0} instead of 1.",
+                          Args.size()));
+  LUTHIER_RETURN_ON_ERROR(LUTHIER_ERROR_CHECK(
+      Args[0].first.isRegDefKind(),
+      "The register argument of luthier::readReg is not a definition."));
   llvm::Register Output = Args[0].second;
   auto &ST = MF.getSubtarget<llvm::GCNSubtarget>();
   auto *TRI = ST.getRegisterInfo();
--- src/lib/tooling_common/intrinsic/WriteExec.cpp
+++ src/lib/tooling_common/intrinsic/WriteExec.cpp
@@ -1,5 +1,5 @@
-#include "common/Error.hpp"
 #include "tooling_common/intrinsic/WriteExec.hpp"
+#include "common/Error.hpp"
 #include <AMDGPUTargetMachine.h>
 #include <SIRegisterInfo.h>
 #include <llvm/IR/Function.h>
@@ -13,7 +13,11 @@ writeExecIRProcessor(const llvm::Function &Intrinsic,
                      const llvm::CallInst &User,
                      const llvm::GCNTargetMachine &TM) {
   // The User must only have 1 operand
-  LUTHIER_RETURN_ON_ERROR(LUTHIER_ASSERTION(User.getNumOperands() != 1));
+  LUTHIER_RETURN_ON_ERROR(
+      LUTHIER_ERROR_CHECK(User.arg_size() == 1,
+                          "Expected one operand to be passed to the "
+                          "luthier::writeExec intrinsic '{0}', got {1}.",
+                          User, User.arg_size()));
   luthier::IntrinsicIRLoweringInfo Out;
   // Set the output's constraint
   Out.setReturnValueInfo(&User, "s");
@@ -34,9 +38,16 @@ llvm::Error writeExecMIRProcessor(
     const std::function<llvm::Register(llvm::MCRegister)> &PhysRegAccessor,
     llvm::DenseMap<llvm::MCRegister, llvm::Register> &PhysRegsToBeOverwritten) {
   // There should be only a single virtual register involved in the operation
-  LUTHIER_RETURN_ON_ERROR(LUTHIER_ASSERTION(Args.size() == 1));
+  LUTHIER_RETURN_ON_ERROR(
+      LUTHIER_ERROR_CHECK(Args.size() == 1,
+                          "Number of virtual register arguments "
+                          "involved in the MIR lowering stage of "
+                          "luthier::writeExec is {0} instead of 1.",
+                          Args.size()));
   // It should be of reg use kind
-  LUTHIER_RETURN_ON_ERROR(LUTHIER_ASSERTION(Args[0].first.isRegUseKind()));
+  LUTHIER_RETURN_ON_ERROR(LUTHIER_ERROR_CHECK(
+      Args[0].first.isRegUseKind(),
+      "The register argument of luthier::writeExec is not a definition."));
   llvm::Register InputReg = Args[0].second;
 
   MIBuilder(llvm::AMDGPU::COPY)
