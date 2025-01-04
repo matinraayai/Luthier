034304089197794e5f3914e0ae6f8283daa536de
matinraayai <30674652+matinraayai@users.noreply.github.com>
2025-01-04 14:51:01 -0500

    Added RTTI-like interface to the hsa::AqlPacket.

--- include/luthier/hsa/AqlPacket.h
+++ include/luthier/hsa/AqlPacket.h
@@ -1,5 +1,5 @@
 //===-- AqlPacket.h - HSA AQL Packet POD Wrapper ----------------*- C++ -*-===//
-// Copyright 2022-2024 @ Northeastern University Computer Architecture Lab
+// Copyright 2022-2025 @ Northeastern University Computer Architecture Lab
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -15,18 +15,17 @@
 //===----------------------------------------------------------------------===//
 ///
 /// \file
-/// This file the \c AqlPacket struct under the \c luthier::hsa namespace,
-/// which is a plain-old-data struct that provides an abstraction over the
-/// AQL packet.
+/// This file describes the \c luthier::hsa::AqlPacket struct,
+/// a plain-old-data struct that provides an abstraction over the
+/// HSA AQL packet.
 //===----------------------------------------------------------------------===//
-
 #ifndef LUTHIER_HSA_AQL_PACKET_H
 #define LUTHIER_HSA_AQL_PACKET_H
 #include <hsa/hsa.h>
 
 namespace luthier::hsa {
 
-/// \brief AMD Vendor Packet POD struct
+/// \brief AMD Vendor Packet struct
 typedef struct {
   uint16_t Header;
   uint8_t Format;
@@ -34,14 +33,9 @@ typedef struct {
 } amd_vendor_packet_t;
 
 /// \brief POD struct to provide an abstraction over HSA AQL packets as well
-/// as some convenience methods
-/// \details To inspect the content of a packet using this struct simply
-/// use use <tt>reinterpret_cast</tt> over the address of the packet:
-/// \code
-/// auto& Packet = *reinterpret_cast<HsaAqlPacket*>(PacketAddress);
-/// \endcode
+/// as some convenience methods to convert them to their specific type
 struct AqlPacket {
-  /// \brief Generic packet struct
+  /// Generic packet struct
   struct {
     uint16_t Header;
     struct {
@@ -57,71 +51,117 @@ struct AqlPacket {
         ((1 << HSA_PACKET_HEADER_WIDTH_TYPE) - 1));
   }
 
-  /// \return a non-const reference to the content of this packet viewed
-  /// as a AMD vendor packet
-  /// \sa hsa_amd_vendor_packet_t
-  [[nodiscard]] amd_vendor_packet_t &asAMDVendor() {
-    return *reinterpret_cast<amd_vendor_packet_t *>(&Packet);
+  /// \return if the type of the packet is \c HSA_PACKET_TYPE_VENDOR_SPECIFIC
+  /// returns a non-const \c amd_vendor_packet_t pointer to the contents of
+  /// the packet; Otherwise, \c nullptr
+  /// \sa amd_vendor_packet_t
+  [[nodiscard]] amd_vendor_packet_t *asAMDVendor() {
+    if (getPacketType() == HSA_PACKET_TYPE_VENDOR_SPECIFIC)
+      return reinterpret_cast<amd_vendor_packet_t *>(&Packet);
+    else
+      return nullptr;
   }
 
-  /// \return a non-const reference to the content of this packet viewed as
-  /// a AMD vendor packet
-  /// \sa hsa_amd_vendor_packet_t
-  [[nodiscard]] const amd_vendor_packet_t &asAMDVendor() const {
-    return *reinterpret_cast<const amd_vendor_packet_t *>(&Packet);
+  /// \return if the type of the packet is \c HSA_PACKET_TYPE_VENDOR_SPECIFIC
+  /// returns a const \c amd_vendor_packet_t pointer to the contents of
+  /// the packet; Otherwise, \c nullptr
+  /// \sa amd_vendor_packet_t
+  [[nodiscard]] const amd_vendor_packet_t *asAMDVendor() const {
+    if (getPacketType() == HSA_PACKET_TYPE_VENDOR_SPECIFIC)
+      return reinterpret_cast<const amd_vendor_packet_t *>(&Packet);
+    else
+      return nullptr;
   }
 
-  /// \return a non-const reference to the contents of this packet viewed as a
-  /// kernel dispatch packet
-  /// \sa hsa_kernel_dispatch_packet_t
-  [[nodiscard]] hsa_kernel_dispatch_packet_t &asKernelDispatch() {
-    return *reinterpret_cast<hsa_kernel_dispatch_packet_t *>(&Packet);
+  /// \return if the type of the packet is \c HSA_PACKET_TYPE_KERNEL_DISPATCH
+  /// returns a non-const \c hsa_kernel_dispatch_packet_t pointer to the
+  /// contents of the packet; Otherwise, \c nullptr
+  /// \sa hsa_kernel_dispatch_packet_t, HSA_PACKET_TYPE_KERNEL_DISPATCH
+  [[nodiscard]] hsa_kernel_dispatch_packet_t *asKernelDispatch() {
+    if (getPacketType() == HSA_PACKET_TYPE_KERNEL_DISPATCH)
+      return reinterpret_cast<hsa_kernel_dispatch_packet_t *>(&Packet);
+    else
+      return nullptr;
   }
 
-  /// \return a const reference to this packet viewed as a kernel dispatch packet
-  /// \sa hsa_kernel_dispatch_packet_t
-  [[nodiscard]] const hsa_kernel_dispatch_packet_t &asKernelDispatch() const {
-    return *reinterpret_cast<const hsa_kernel_dispatch_packet_t *>(&Packet);
+  /// \return if the type of the packet is \c HSA_PACKET_TYPE_KERNEL_DISPATCH
+  /// returns a const \c hsa_kernel_dispatch_packet_t pointer to the
+  /// contents of the packet; Otherwise, \c nullptr
+  /// \sa hsa_kernel_dispatch_packet_t, HSA_PACKET_TYPE_KERNEL_DISPATCH
+  [[nodiscard]] const hsa_kernel_dispatch_packet_t *asKernelDispatch() const {
+    if (getPacketType() == HSA_PACKET_TYPE_KERNEL_DISPATCH)
+      return reinterpret_cast<const hsa_kernel_dispatch_packet_t *>(&Packet);
+    else
+      return nullptr;
   }
 
-  /// \return a non-const reference to this packet viewed as a Barrier-And packet
-  /// \sa hsa_barrier_and_packet_t
-  [[nodiscard]] hsa_barrier_and_packet_t &asBarrierAnd() {
-    return *reinterpret_cast<hsa_barrier_and_packet_t *>(&Packet);
+  /// \return if the type of the packet is \c HSA_PACKET_TYPE_BARRIER_AND
+  /// returns a non-const \c hsa_barrier_and_packet_t pointer to the
+  /// contents of the packet; Otherwise, \c nullptr
+  /// \sa hsa_barrier_and_packet_t, HSA_PACKET_TYPE_BARRIER_AND
+  [[nodiscard]] hsa_barrier_and_packet_t *asBarrierAnd() {
+    if (getPacketType() == HSA_PACKET_TYPE_BARRIER_AND)
+      return reinterpret_cast<hsa_barrier_and_packet_t *>(&Packet);
+    else
+      return nullptr;
   }
 
-  /// \return a const reference to this packet viewed as a Barrier-And packet
-  /// \sa hsa_barrier_and_packet_t
-  [[nodiscard]] const hsa_barrier_and_packet_t &asBarrierAnd() const {
-    return *reinterpret_cast<const hsa_barrier_and_packet_t *>(&Packet);
+  /// \return if the type of the packet is \c HSA_PACKET_TYPE_BARRIER_AND
+  /// returns a const \c hsa_barrier_and_packet_t pointer to the
+  /// contents of the packet; Otherwise, \c nullptr
+  /// \sa hsa_barrier_and_packet_t, HSA_PACKET_TYPE_BARRIER_AND
+  [[nodiscard]] const hsa_barrier_and_packet_t *asBarrierAnd() const {
+    if (getPacketType() == HSA_PACKET_TYPE_BARRIER_AND)
+      return reinterpret_cast<const hsa_barrier_and_packet_t *>(&Packet);
+    else
+      return nullptr;
   }
 
-  /// \return a non-const reference to this packet viewed as a Barrier-Or packet
-  /// \sa hsa_barrier_or_packet_t
-  [[nodiscard]] hsa_barrier_or_packet_t &asBarrierOr() {
-    return *reinterpret_cast<hsa_barrier_or_packet_t *>(&Packet);
+  /// \return if the type of the packet is \c HSA_PACKET_TYPE_BARRIER_OR
+  /// returns a non-const \c hsa_barrier_or_packet_t pointer to the
+  /// contents of the packet; Otherwise, \c nullptr
+  /// \sa hsa_barrier_or_packet_t, HSA_PACKET_TYPE_BARRIER_OR
+  [[nodiscard]] hsa_barrier_or_packet_t *asBarrierOr() {
+    if (getPacketType() == HSA_PACKET_TYPE_BARRIER_OR)
+      return reinterpret_cast<hsa_barrier_or_packet_t *>(&Packet);
+    else
+      return nullptr;
   }
 
-  /// \return a const reference to this packet viewed as a Barrier-Or packet
-  /// \sa hsa_barrier_or_packet_t
-  [[nodiscard]] const hsa_barrier_or_packet_t &asBarrierOr() const {
-    return *reinterpret_cast<const hsa_barrier_or_packet_t *>(&Packet);
+  /// \return if the type of the packet is \c HSA_PACKET_TYPE_BARRIER_OR
+  /// returns a const \c hsa_barrier_or_packet_t pointer to the
+  /// contents of the packet; Otherwise, \c nullptr
+  /// \sa hsa_barrier_or_packet_t, HSA_PACKET_TYPE_BARRIER_OR
+  [[nodiscard]] const hsa_barrier_or_packet_t *asBarrierOr() const {
+    if (getPacketType() == HSA_PACKET_TYPE_BARRIER_OR)
+      return reinterpret_cast<const hsa_barrier_or_packet_t *>(&Packet);
+    else
+      return nullptr;
   }
 
-  /// \return a non-const reference to this packet viewed as a Agent packet
-  /// \sa hsa_agent_dispatch_packet_t
-  [[nodiscard]] hsa_agent_dispatch_packet_t &asAgentDispatch() {
-    return *reinterpret_cast<hsa_agent_dispatch_packet_t *>(&Packet);
+  /// \return if the type of the packet is \c HSA_PACKET_TYPE_AGENT_DISPATCH
+  /// returns a non-const \c hsa_agent_dispatch_packet_t pointer to the
+  /// contents of the packet; Otherwise, \c nullptr
+  /// \sa hsa_agent_dispatch_packet_t, HSA_PACKET_TYPE_AGENT_DISPATCH
+  [[nodiscard]] hsa_agent_dispatch_packet_t *asAgentDispatch() {
+    if (getPacketType() == HSA_PACKET_TYPE_AGENT_DISPATCH)
+      return reinterpret_cast<hsa_agent_dispatch_packet_t *>(&Packet);
+    else
+      return nullptr;
   }
 
-  /// \return a const reference to this packet viewed as a Agent packet
-  /// \sa hsa_agent_dispatch_packet_t
-  [[nodiscard]] const hsa_agent_dispatch_packet_t &asAgentDispatch() const {
-    return *reinterpret_cast<const hsa_agent_dispatch_packet_t *>(&Packet);
+  /// \return if the type of the packet is \c HSA_PACKET_TYPE_AGENT_DISPATCH
+  /// returns a const \c hsa_agent_dispatch_packet_t pointer to the
+  /// contents of the packet; Otherwise, \c nullptr
+  /// \sa hsa_agent_dispatch_packet_t, HSA_PACKET_TYPE_AGENT_DISPATCH
+  [[nodiscard]] const hsa_agent_dispatch_packet_t *asAgentDispatch() const {
+    if (getPacketType() == HSA_PACKET_TYPE_AGENT_DISPATCH)
+      return reinterpret_cast<const hsa_agent_dispatch_packet_t *>(&Packet);
+    else
+      return nullptr;
   }
 };
 
-} // namespace hsa
-
+} // namespace luthier::hsa
 
 #endif
