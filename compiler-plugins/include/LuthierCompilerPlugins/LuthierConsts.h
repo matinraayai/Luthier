fa7278f039560416ce783443ee6defe4557b89ef
matinraayai <30674652+matinraayai@users.noreply.github.com>
2025-01-21 13:13:54 -0500

    Compiler plugins: - Moved all consts under the LuthierConsts.h header file.

--- compiler-plugins/include/LuthierCompilerPlugins/LuthierConsts.h
+++ compiler-plugins/include/LuthierCompilerPlugins/LuthierConsts.h
@@ -24,48 +24,58 @@
 
 namespace luthier {
 
+//===----------------------------------------------------------------------===//
+// Utility macros (see https://github.com/pfultz2/Cloak)
+//===----------------------------------------------------------------------===//
+#define LUTHIER_STRINGIFY(S) LUTHIER_PRIMITIVE_STR(S)
+#define LUTHIER_PRIMITIVE_STR(S) #S
+
+#define LUTHIER_CAT(a, ...) LUTHIER_PRIMITIVE_CAT(a, __VA_ARGS__)
+#define LUTHIER_PRIMITIVE_CAT(a, ...) a##__VA_ARGS__
+
+//===----------------------------------------------------------------------===//
+// Luthier attributes and constants
+//===----------------------------------------------------------------------===//
+
 /// Prefix appended to all hook handle kernels
 #define LUTHIER_HOOK_HANDLE_PREFIX __luthier_hook_handle_
 
 /// All hooks in instrumentation modules must have this attribute
-#define LUTHIER_HOOK_ATTRIBUTE "luthier_hook"
+#define LUTHIER_HOOK_ATTRIBUTE luthier_hook
 
 /// Name of the reserved managed variable defined in all Luthier tools so
 /// that its device module can be easily identified at runtime
 #define LUTHIER_RESERVED_MANAGED_VAR __luthier_reserved
 
-
 /// All bindings to Luthier intrinsics must have this attribute
-#define LUTHIER_INTRINSIC_ATTRIBUTE "luthier_intrinsic"
+#define LUTHIER_INTRINSIC_ATTRIBUTE luthier_intrinsic
+
+/// Prefix of the CUID symbol inside a HIP module
+#define LUTHIER_HIP_CUID_PREFIX __hip_cuid_
 
 /// All injected payload functions during instrumentation (i.e. functions that
 /// their machine code will be inserted before an instrumentation point) must
 /// have this attribute
-#define LUTHIER_INJECTED_PAYLOAD_ATTRIBUTE "luthier_injected_payload"
+#define LUTHIER_INJECTED_PAYLOAD_ATTRIBUTE luthier_injected_payload
 
-#define STRINGIFY(S) #S
+static constexpr const char *HookHandlePrefix =
+    LUTHIER_STRINGIFY(LUTHIER_HOOK_HANDLE_PREFIX);
 
-/// A macro to delay expansion of \c STRINGIFY
-#define STRINGIFY_DELAY(S) STRINGIFY(S)
+static constexpr const char *ReservedManagedVar =
+    LUTHIER_STRINGIFY(LUTHIER_RESERVED_MANAGED_VAR);
 
-constexpr const char *HookHandlePrefix = STRINGIFY_DELAY(LUTHIER_HOOK_HANDLE_PREFIX);
+static constexpr const char *HipCUIDPrefix =
+    LUTHIER_STRINGIFY(LUTHIER_HIP_CUID_PREFIX);
 
-constexpr const char *ReservedManagedVar =
-    STRINGIFY_DELAY(LUTHIER_RESERVED_MANAGED_VAR);
+static constexpr const char *HookAttribute =
+    LUTHIER_STRINGIFY(LUTHIER_HOOK_ATTRIBUTE);
 
-static constexpr const char *HipCUIDPrefix = "__hip_cuid_";
+static constexpr const char *IntrinsicAttribute =
+    LUTHIER_STRINGIFY(LUTHIER_INTRINSIC_ATTRIBUTE);
 
+static constexpr const char *InjectedPayloadAttribute =
+    LUTHIER_STRINGIFY(LUTHIER_INJECTED_PAYLOAD_ATTRIBUTE);
 
-#undef STRINGIFY_DELAY
-
-#undef STRINGIFY
-
-static constexpr const char *HookAttribute = "luthier_hook";
-
-static constexpr const char *IntrinsicAttribute = "luthier_intrinsic";
-
-
-
-}
+} // namespace luthier
 
 #endif
