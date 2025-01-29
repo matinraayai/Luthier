520f1772dd2776f25dd34d0b03a1fa5a86710ae6
matinraayai <30674652+matinraayai@users.noreply.github.com>
2025-01-29 15:46:20 -0500

    Fixed incorrect include in amd comgr.

--- include/luthier/comgr/ComgrError.h
+++ include/luthier/comgr/ComgrError.h
@@ -1,11 +1,11 @@
 #ifndef LUTHIER_COMGR_ERROR_H
 #define LUTHIER_COMGR_ERROR_H
-#include "luthier/common/ROCmLibraryErrorDefine.h"
 #include <amd_comgr/amd_comgr.h>
 #include <llvm/Support/Error.h>
 
 namespace luthier {
 
+#include "luthier/common/ROCmLibraryErrorDefine.h"
 LUTHIER_DEFINE_ROCM_LIBRARY_ERROR(Comgr, amd_comgr_status_t,
                                   AMD_COMGR_STATUS_SUCCESS);
 
@@ -17,6 +17,8 @@ LUTHIER_DEFINE_ROCM_LIBRARY_ERROR(Comgr, amd_comgr_status_t,
 /// Macro to check for success of a Comgr operation
 #define LUTHIER_COMGR_SUCCESS_CHECK(Expr)                                      \
   luthier::ComgrError::ComgrErrorCheck(__FILE__, __LINE__, Expr, #Expr)
+
+#undef LUTHIER_DEFINE_ROCM_LIBRARY_ERROR
 
 } // namespace luthier
 
