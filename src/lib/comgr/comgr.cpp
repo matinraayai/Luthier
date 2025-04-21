71302cd568ce1b5229c348f10e78c64b9e521d73
matinraayai <30674652+matinraayai@users.noreply.github.com>
2025-04-21 13:15:04 -0400

    Comgr: - Moved linking from code generator to its own function. - Added LIT executable test for testing the linking functionality. - Added LIT test directory.

---
+++ src/lib/comgr/comgr.cpp
@@ -0,0 +1,64 @@
+
+#include "comgr/comgr.hpp"
+#include <amd_comgr/amd_comgr.h>
+#include <llvm/Support/TimeProfiler.h>
+#include <luthier/comgr/ComgrError.h>
+#include <luthier/common/ErrorCheck.h>
+
+namespace luthier::comgr {
+
+llvm::Error linkRelocatableToExecutable(llvm::ArrayRef<char> Code,
+                                        llvm::SmallVectorImpl<char> &Out) {
+  llvm::TimeTraceScope Scope("Comgr Executable Linking");
+  amd_comgr_data_t DataIn;
+  amd_comgr_data_set_t DataSetIn, DataSetOut;
+  amd_comgr_action_info_t DataAction;
+
+  LUTHIER_RETURN_ON_ERROR(
+      LUTHIER_COMGR_SUCCESS_CHECK(amd_comgr_create_data_set(&DataSetIn)));
+
+  LUTHIER_RETURN_ON_ERROR(LUTHIER_COMGR_SUCCESS_CHECK(
+      amd_comgr_create_data(AMD_COMGR_DATA_KIND_RELOCATABLE, &DataIn)));
+
+  LUTHIER_RETURN_ON_ERROR(LUTHIER_COMGR_SUCCESS_CHECK(
+      amd_comgr_set_data(DataIn, Code.size(), Code.data())));
+
+  LUTHIER_RETURN_ON_ERROR(LUTHIER_COMGR_SUCCESS_CHECK(
+      (amd_comgr_set_data_name(DataIn, "source.o"))));
+
+  LUTHIER_RETURN_ON_ERROR(
+      LUTHIER_COMGR_SUCCESS_CHECK((amd_comgr_data_set_add(DataSetIn, DataIn))));
+
+  LUTHIER_RETURN_ON_ERROR(
+      LUTHIER_COMGR_SUCCESS_CHECK((amd_comgr_create_data_set(&DataSetOut))));
+
+  LUTHIER_RETURN_ON_ERROR(
+      LUTHIER_COMGR_SUCCESS_CHECK((amd_comgr_create_action_info(&DataAction))));
+
+  // LUTHIER_RETURN_ON_ERROR(LUTHIER_COMGR_SUCCESS_CHECK(
+      // (amd_comgr_action_info_set_isa_name(DataAction, IsaName->c_str()))));
+  const char *LinkOptions[]{"-Wl,--unresolved-symbols=ignore-all"};
+  LUTHIER_RETURN_ON_ERROR(LUTHIER_COMGR_SUCCESS_CHECK(
+      (amd_comgr_action_info_set_option_list(DataAction, LinkOptions, 1))));
+  LUTHIER_RETURN_ON_ERROR(LUTHIER_COMGR_SUCCESS_CHECK(
+      (amd_comgr_do_action(AMD_COMGR_ACTION_LINK_RELOCATABLE_TO_EXECUTABLE,
+                           DataAction, DataSetIn, DataSetOut))));
+
+  amd_comgr_data_t DataOut;
+  size_t DataOutSize;
+  LUTHIER_RETURN_ON_ERROR(
+      LUTHIER_COMGR_SUCCESS_CHECK((amd_comgr_action_data_get_data(
+          DataSetOut, AMD_COMGR_DATA_KIND_EXECUTABLE, 0, &DataOut))));
+  LUTHIER_RETURN_ON_ERROR(LUTHIER_COMGR_SUCCESS_CHECK(
+      (amd_comgr_get_data(DataOut, &DataOutSize, nullptr))));
+  Out.resize(DataOutSize);
+  LUTHIER_RETURN_ON_ERROR(LUTHIER_COMGR_SUCCESS_CHECK((amd_comgr_get_data(
+      DataOut, &DataOutSize, Out.data()))));
+  LUTHIER_RETURN_ON_ERROR(
+      LUTHIER_COMGR_SUCCESS_CHECK(amd_comgr_destroy_data_set(DataSetIn)));
+  LUTHIER_RETURN_ON_ERROR(
+      LUTHIER_COMGR_SUCCESS_CHECK(amd_comgr_destroy_data_set(DataSetOut)));
+  return llvm::Error::success();
+}
+
+} // namespace luthier::comgr
---
+++ src/test/lit/comgr/comgr-link.cpp
@@ -0,0 +1,75 @@
+//===-- comgr-link.cpp ----------------------------------------------------===//
+// Copyright 2022-2025 @ Northeastern University Computer Architecture Lab
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//===----------------------------------------------------------------------===//
+///
+/// \file
+/// This file implements comgr-link, an executable used to test the relocatable
+/// linking functionality of Comgr used in Luthier.
+//===----------------------------------------------------------------------===//
+#include "comgr/comgr.hpp"
+#include <llvm/ADT/StringExtras.h>
+#include <llvm/Support/CommandLine.h>
+#include <llvm/Support/FileSystem.h>
+#include <llvm/Support/InitLLVM.h>
+#include <llvm/Support/MemoryBuffer.h>
+#include <llvm/Support/ToolOutputFile.h>
+#include <llvm/Support/WithColor.h>
+#include <luthier/common/ErrorCheck.h>
+#include <luthier/common/LuthierError.h>
+
+static llvm::cl::OptionCategory ComgrLinkOptions("Comgr Link Options");
+
+static llvm::cl::opt<std::string>
+    InputFilename(llvm::cl::Positional, llvm::cl::desc("<input file>"),
+                  llvm::cl::init("-"), llvm::cl::cat(ComgrLinkOptions));
+
+static llvm::cl::opt<std::string>
+    OutputFilename("o", llvm::cl::desc("Output filename"),
+                   llvm::cl::value_desc("filename"), llvm::cl::init("-"),
+                   llvm::cl::cat(ComgrLinkOptions));
+
+int main(int Argc, char *Argv[]) {
+  llvm::InitLLVM X(Argc, Argv);
+
+  llvm::cl::HideUnrelatedOptions(
+      {&ComgrLinkOptions, &llvm::getColorCategory()});
+  llvm::cl::ParseCommandLineOptions(Argc, Argv,
+                                    "Luthier link with comgr tool\n");
+
+  llvm::ErrorOr<std::unique_ptr<llvm::MemoryBuffer>> BufferPtr =
+      llvm::MemoryBuffer::getFileOrSTDIN(InputFilename);
+
+  std::error_code EC = BufferPtr.getError();
+  LUTHIER_REPORT_FATAL_ON_ERROR(LUTHIER_ERROR_CHECK(
+      !EC.operator bool(), "Failed to open input file, error: {0}.",
+      EC.message()));
+
+  llvm::StringRef Buffer = BufferPtr->get()->getBuffer();
+
+  llvm::SmallVector<char> Executable;
+  LUTHIER_REPORT_FATAL_ON_ERROR(luthier::comgr::linkRelocatableToExecutable(
+      llvm::arrayRefFromStringRef<char>(Buffer), Executable));
+
+  auto OutFile = std::make_unique<llvm::ToolOutputFile>(OutputFilename, EC,
+                                                        llvm::sys::fs::OF_None);
+  LUTHIER_REPORT_FATAL_ON_ERROR(LUTHIER_ERROR_CHECK(
+      !EC.operator bool(), "Failed to open output file, error: {0}.",
+      EC.message()));
+  OutFile->os() << Executable;
+
+  OutFile->keep();
+
+  return 0;
+}
