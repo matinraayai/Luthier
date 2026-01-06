7de7da0e45a6f7b12c3a1726ec59a0f060c17b82
matinraayai <30674652+matinraayai@users.noreply.github.com>
2026-01-06 15:43:24 -0500

    Compilation issue fix.

--- src/lib/ToolingCommon/PseudoOpcodeAndRegMapper.cpp
+++ src/lib/ToolingCommon/PseudoOpcodeAndRegMapper.cpp
@@ -1,8 +1,10 @@
 #include "luthier/Tooling/PseudoOpcodeAnRegMapper.h"
+#include <AMDGPUTargetMachine.h>
 
 namespace luthier {
 
 #include "LuthierRealToPseudoOpcodeMap.hpp"
+
 #include "LuthierRealToPseudoRegEnumMap.hpp"
 
 } // namespace luthier
