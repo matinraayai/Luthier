28da7fd8c87fde2665f46212b0013530caabf654
matinraayai <30674652+matinraayai@users.noreply.github.com>
2026-01-06 15:39:38 -0500

    Compilation issue fix.

--- include/luthier/Tooling/PseudoOpcodeAnRegMapper.h
+++ include/luthier/Tooling/PseudoOpcodeAnRegMapper.h
@@ -2,11 +2,14 @@
 
 #ifndef LUTHIER_TOOLING_PSEUDO_OPCODE_AN_REG_MAPPER_H
 #define LUTHIER_TOOLING_PSEUDO_OPCODE_AN_REG_MAPPER_H
+#include <llvm/Support/Compiler.h>
 
 namespace luthier {
 
+LLVM_READONLY
 unsigned short getPseudoOpcodeFromReal(unsigned short Opcode);
 
+LLVM_READONLY
 unsigned short RealToPseudoRegisterMapTable(unsigned short Reg);
 
 } // namespace luthier
