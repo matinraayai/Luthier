04fce65afe84160901614c2831c99ad7f5760f95
matinraayai <30674652+matinraayai@users.noreply.github.com>
2026-01-10 13:17:49 -0500

    Minor changes.

--- src/lib/ToolingCommon/MockLoadAMDGPUCodeObjects.cpp
+++ src/lib/ToolingCommon/MockLoadAMDGPUCodeObjects.cpp
@@ -13,7 +13,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 //===----------------------------------------------------------------------===//
-/// \file
+/// \file MockLoadAMDGPUCodeObjects.cpp
 /// Implements the \c MockLoadAMDGPUCodeObjects class.
 //===----------------------------------------------------------------------===//
 #include "luthier/Tooling/MockLoadAMDGPUCodeObjects.h"
