30adb4dfa2069ad272354a9297a03474f3c3148a
Iasonaskrpr <iaskarapro@gmail.com>
2026-01-20 10:59:08 -0500

    Added some functions from llvm's implementation of branch relaxation

--- include/luthier/Tooling/BranchRelaxationPass.h
+++ include/luthier/Tooling/BranchRelaxationPass.h
@@ -2,23 +2,72 @@
 #ifndef LUTHIER_TOOLING_BRANCH_RELAXATION_PASS_H
 #define LUTHIER_TOOLING_BRANCH_RELAXATION_PASS_H
 
-#include <llvm/IR/PassManager.h>
+#include <llvm/CodeGen/MachineFunction.h>
 #include <llvm/CodeGen/RegisterScavenging.h>
+#include <llvm/CodeGen/MachineBasicBlock.h>
 #include <llvm/CodeGen/TargetInstrInfo.h>
 #include <llvm/CodeGen/TargetRegisterInfo.h>
 #include <llvm/CodeGen/TargetSubtargetInfo.h>
-#include <llvm/CodeGen/MachineFunction.h>
+#include <llvm/IR/PassManager.h>
 
-namespace luthier{
+namespace luthier {
 
-    class BranchRelaxationPass : public PassInfoMixin<BranchRelaxationPass> {
-        llvm::MachineFunction *MF = nullptr;
-        const llvm::TargetRegisterInfo *TRI = nullptr;
-        const llvm::TargetInstrInfo *TII = nullptr;
-        const llvm::TargetMachine *TM = nullptr;
-        public:
-            llvm::PreservedAnalyses run(llvm::Module &Module, llvm::ModuleAnalysisManager &MAM);
-    };
-}
+class BranchRelaxationPass : public PassInfoMixin<BranchRelaxationPass> {
+  /// BasicBlockInfo - Information about the offset and size of a single
+  /// basic block.
+  struct BasicBlockInfo {
+    /// Offset - Distance from the beginning of the function to the beginning
+    /// of this basic block.
+    ///
+    /// The offset is always aligned as required by the basic block.
+    unsigned Offset = 0;
 
-#endif 
+    /// Size - Size of the basic block in bytes.  If the block contains
+    /// inline assembly, this is a worst case estimate.
+    ///
+    /// The size does not include any alignment padding whether from the
+    /// beginning of the block, or from an aligned jump table at the end.
+    unsigned Size = 0;
+
+    BasicBlockInfo() = default;
+
+    /// Compute the offset immediately following this block. \p MBB is the next
+    /// block.
+    unsigned postOffset(const MachineBasicBlock &MBB) const {
+      const unsigned PO = Offset + Size;
+      const Align Alignment = MBB.getAlignment();
+      const Align ParentAlign = MBB.getParent()->getAlignment();
+      if (Alignment <= ParentAlign)
+        return alignTo(PO, Alignment);
+
+      // The alignment of this MBB is larger than the function's alignment, so
+      // we can't tell whether or not it will insert nops. Assume that it will.
+      return alignTo(PO, Alignment) + Alignment.value() - ParentAlign.value();
+    }
+  };
+
+  SmallVector<BasicBlockInfo, 16> BlockInfo;
+  // The basic block after which trampolines are inserted. This is the last
+  // basic block that isn't in the cold section.
+  llvm::MachineBasicBlock *TrampolineInsertionPoint = nullptr;
+  llvm::SmallDenseSet<std::pair<llvm::MachineBasicBlock *, llvm::MachineBasicBlock *>> RelaxedUnconditionals;
+  std::unique_ptr<llvm::RegScavenger> RS(new llvm::RegScavenger());
+  llvm::LivePhysRegs LiveRegs;
+  llvm::MachineFunction *MF = nullptr;
+  const llvm::TargetRegisterInfo *TRI = nullptr;
+  const llvm::TargetInstrInfo *TII = nullptr;
+  const llvm::TargetMachine *TM = nullptr;
+  
+  bool BranchRelaxationPass::relaxBranchInstructions();
+  void scanFunction();
+  uint64_t computeBlockSize(const llvm::MachineBasicBlock &MBB) const;
+  
+
+public:
+  
+  llvm::PreservedAnalyses run(llvm::MachineFunction &TargetMF,
+                              llvm::MachineFunctionAnalysisManager &TargetMFAM);
+};
+} // namespace luthier
+
+#endif
--- src/lib/ToolingCommon/BranchRelaxationPass.cpp
+++ src/lib/ToolingCommon/BranchRelaxationPass.cpp
@@ -1,30 +1,151 @@
 //===----------------------------------------------------------------------===//
 ///
 /// \file
-/// This file implements a branch relaxation pass for the code we are instrumenting
+/// This file implements a branch relaxation pass for the code we are
+/// instrumenting
 //===----------------------------------------------------------------------===//
 
-#include <luthier/Tooling/BranchRelaxationPass.h>
 #include <llvm/CodeGen/LivePhysRegs.h>
 #include <llvm/CodeGen/MachineBasicBlock.h>
 #include <llvm/CodeGen/MachineInstr.h>
 #include <llvm/IR/PassManager.h>
 #include <llvm/Target/TargetMachine.h>
+#include <luthier/Tooling/BranchRelaxationPass.h>
 #undef DEBUG_TYPE
 
 #define DEBUG_TYPE "luthier-branch-relaxation"
 
-namespace luthier{
+namespace luthier {
 
-    llvm::PreservedAnalyses BranchRelaxationPass::run(llvm::MachineFunction &TargetMF, llvm::MachineFunctionAnalysisManager &TargetMFAM) {
-        MF = &TargetMF;
-        bool MadeChanges;
-        LLVM_DEBUG(dbgs() << "***** BranchRelaxation *****\n");
+bool BranchRelaxationPass::relaxBranchInstructions() {
+  bool Changed = false;
 
-        
-        const llvm::TargetSubtargetInfo &ST = TargetMF.getSubtarget();
-        if(MadeChanges) return llvm::PreservedAnalyses::none();
-        return llvm::PreservedAnalyses::all();
+  // Relaxing branches involves creating new basic blocks, so re-eval
+  // end() for termination.
+  for (llvm::MachineBasicBlock &MBB : *MF) {
+    llvm::MachineBasicBlock::iterator Last = MBB.getLastNonDebugInstr();
+    if (Last == MBB.end())
+      continue;
+
+    // Expand the unconditional branch first if necessary. If there is a
+    // conditional branch, this will end up changing the branch destination of
+    // it to be over the newly inserted indirect branch block, which may avoid
+    // the need to try expanding the conditional branch first, saving an extra
+    // jump.
+    if (Last->isUnconditionalBranch()) {
+      // Unconditional branch destination might be unanalyzable, assume these
+      // are OK.
+      if (llvm::MachineBasicBlock *DestBB = TII->getBranchDestBlock(*Last)) {
+        if (!isBlockInRange(*Last, *DestBB) && !TII->isTailCall(*Last) &&
+            !RelaxedUnconditionals.contains({&MBB, DestBB})) {
+          fixupUnconditionalBranch(*Last);
+          ++NumUnconditionalRelaxed;
+          Changed = true;
+        }
+      }
     }
+
+    // Loop over the conditional branches.
+    llvm::MachineBasicBlock::iterator Next;
+    for (llvm::MachineBasicBlock::iterator J = MBB.getFirstTerminator();
+         J != MBB.end(); J = Next) {
+      Next = std::next(J);
+      llvm::MachineInstr &MI = *J;
+
+      if (!MI.isConditionalBranch())
+        continue;
+
+      if (MI.getOpcode() == llvm::TargetOpcode::FAULTING_OP)
+        // FAULTING_OP's destination is not encoded in the instruction stream
+        // and thus never needs relaxed.
+        continue;
+
+      llvm::MachineBasicBlock *DestBB = TII->getBranchDestBlock(MI);
+      if (!isBlockInRange(MI, *DestBB)) {
+        if (Next != MBB.end() && Next->isConditionalBranch()) {
+          // If there are multiple conditional branches, this isn't an
+          // analyzable block. Split later terminators into a new block so
+          // each one will be analyzable.
+
+          splitBlockBeforeInstr(*Next, DestBB);
+        } else {
+          fixupConditionalBranch(MI);
+          ++NumConditionalRelaxed;
+        }
+
+        Changed = true;
+
+        // This may have modified all of the terminators, so start over.
+        Next = MBB.getFirstTerminator();
+      }
+    }
+  }
+
+  // If we relaxed a branch, we must recompute offsets for all basic blocks.
+  // Otherwise, we may underestimate branch distances and fail to relax a branch
+  // that has been pushed out of range.
+  if (Changed)
+    adjustBlockOffsets(MF->front());
+
+  return Changed;
+}
+
+void BranchRelaxationPass::scanFunction() {
+  BlockInfo.clear();
+  BlockInfo.resize(MF->getNumBlockIDs());
+
+  TrampolineInsertionPoint = nullptr;
+  RelaxedUnconditionals.clear();
+
+  // Compute the size of all basic blocks, the last basic block of the function
+  // is the trampoline insertion point
+  for (llvm::MachineBasicBlock &MBB : *MF) {
+    BlockInfo[MBB.getNumber()].Size = computeBlockSize(MBB);
+    TrampolineInsertionPoint = &MBB;
+  }
+
+  // Compute block offsets and known bits.
+  adjustBlockOffsets(*MF->begin());
+
+  if (TrampolineInsertionPoint == nullptr) {
+    LLVM_DEBUG(dbgs() << "  No suitable trampoline insertion point found in "
+                      << MF->getName() << ".\n");
+  }
+}
+/// Compute the size of the basic block in bytes
+uint64_t BranchRelaxationPass::computeBlockSize(
+    const llvm::MachineBasicBlock &MBB) const {
+  uint64_t Size = 0;
+  for (const llvm::MachineInstr &MI : MBB)
+    Size += TII->getInstSizeInBytes(MI);
+  return Size;
+}
+
+llvm::PreservedAnalyses
+BranchRelaxationPass::run(llvm::MachineFunction &TargetMF,
+                          llvm::MachineFunctionAnalysisManager &TargetMFAM) {
+  MF = &TargetMF;
+  bool MadeChanges = false;
+  LLVM_DEBUG(dbgs() << "***** BranchRelaxation *****\n");
+
+  const llvm::TargetSubtargetInfo &ST = TargetMF.getSubtarget();
+  TII = ST.getInstrInfo();
+
+  MF->RenumberBlocks();
+
+  // We scan the machine function to compute information about the basic blocks
+  scanFunction();
+    
+  while(relaxBranchInstructions())
+    MadeChanges = true;
+
+
+  BlockInfo.clear();
+  RelaxedUnconditionals.clear();
+
+  if (MadeChanges)
+    return llvm::PreservedAnalyses::none();
+  return llvm::PreservedAnalyses::all();
+}
 
 } // namespace luthier
