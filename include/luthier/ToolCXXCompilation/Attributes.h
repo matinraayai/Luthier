8ed36c8649dbdf0355cd9db7a3ec8f73cb83c841
Matin Raayai Ardakani <raayaiardakani.m@northeastern.edu>
2026-07-01 16:42:13 +0000

    Removed the now-defunct host handle export attribute.

--- include/luthier/ToolCXXCompilation/Attributes.h
+++ include/luthier/ToolCXXCompilation/Attributes.h
@@ -22,47 +22,7 @@
 
 namespace luthier {
 
-/// \brief Handles the \c [[luthier::export_function_handle]] attribute.
-///
-/// If applied to a \c __device__ function (concrete or templated), this
-/// attribute (with the help of the \c ExportDevFuncHostHandleConsumer) ensures
-/// that a sibling \c __host__ overload of it is present for use within the host
-/// portion of the translation unit.
-///
-/// The attribute parser performs the following actions after encountering an
-/// annotated \c __device__ function:
-///   - If the (templated) function's declaration is not \c __host__ and
-///   does not have an associated \c __host__ declaration or definition yet
-///   defined, the declaration is first cloned. The cloned declaration is then
-///   given the \c __host__ attribute and annotated with both
-///   \c ExportFunctionHandleAutoGenMarker and \c ExportFunctionHandleMarker.
-///   The \c ExportFunctionHandleAutoGenMarker indicates to
-///   \c ExportDevFuncHostHandleConsumer that this declaration was synthesized
-///   and should be treated accordingly.
-///   - If the (templated) function's declaration is also \c __host__, the
-///   function itself will only be annotated with \c ExportFunctionHandleMarker.
-///   - If the (templated) function's declaration already has a \c __host__
-///   version declared, both the \c __device__ and \c __host__ declarations will
-///   be marked with the \c ExportFunctionHandleMarker annotation.
-///
-/// All other usage of this annotation is ignored, and no diagnostics will be
-/// emitted when this happens.
-/// \note Presence of this attribute to a \c __device__ function does not
-/// imply that the exported host function handle will not be
-/// Dead-Code Eliminated. An explicit \c used attribute must be used with the
-/// \c __device__ function and its \c __host__ handle to prevent that from
-/// happening.
-/// \sa ExportDevFuncHostHandleConsumer
-struct LuthierExportFunctionHandleAttrInfo : public clang::ParsedAttrInfo {
-  LuthierExportFunctionHandleAttrInfo();
-
-  bool diagAppertainsToDecl(clang::Sema &S, const clang::ParsedAttr &Attr,
-                            const clang::Decl *D) const override;
-
-  AttrHandling
-  handleDeclAttribute(clang::Sema &S, clang::Decl *D,
-                      const clang::ParsedAttr &Attr) const override;
-};
+/// TODO: Define an attribute that will add an LLVM inreg annotation
 
 } // namespace luthier
 
